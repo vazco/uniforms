75bd524ea5989c121fc1ecc4ef00e7168826d141
alan bount <alan@zeroasterisk.com>
2016-05-23 18:13:47 -0400

    styled ErrorsField

--- packages/uniforms-bootstrap4/src/components/fields/ErrorsField.js
+++ packages/uniforms-bootstrap4/src/components/fields/ErrorsField.js
@@ -4,16 +4,23 @@ import {BaseField} from 'uniforms';
 
 const ErrorsField = ({className, children, ...props}, {uniforms: {error, schema}}) =>
     (!error && !children) ? null : (
-        <section className={classnames('ui', className, 'error message')} {...props}>
-            {children}
-
-            <ul className="list">
-                {schema.getErrorMessages(error).map((message, index) =>
-                    <li key={index}>
-                        {message}
-                    </li>
-                )}
-            </ul>
+        <section className={classnames(
+          'card card-inverse card-danger text-xs-left',
+          className,
+          'error message'
+        )} {...props}>
+            <div className="card-block">
+                <div className="card-text">
+                    {children}
+                    <ul className="list">
+                        {schema.getErrorMessages(error).map((message, index) =>
+                            <li key={index}>
+                                {message}
+                            </li>
+                        )}
+                    </ul>
+                </div>
+            </div>
         </section>
     )
 ;
