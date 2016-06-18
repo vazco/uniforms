5b7fa6077806a70a5c2da29d733249e11571a3c4
Radosław Miernik <radekmie@gmail.com>
2016-06-18 18:35:13 +0200

    Multiple checkboxes in SelectField (closes #33).

--- packages/uniforms-semantic/src/components/fields/SelectField.js
+++ packages/uniforms-semantic/src/components/fields/SelectField.js
@@ -2,10 +2,65 @@ import React          from 'react';
 import classnames     from 'classnames';
 import {connectField} from 'uniforms';
 
+const xor = (item, array) => {
+    let index = array.indexOf(item);
+    if (index === -1) {
+        return array.concat([item]);
+    }
+
+    return array.slice(0, index).concat(array.slice(index + 1));
+};
+
+const renderCheckboxes = ({allowedValues, disabled, fieldType, id, name, onChange, transform, value}) =>
+    allowedValues.map(item =>
+        <section className="field" key={item}>
+            <section className="ui checkbox">
+                <input
+                    checked={fieldType === Array ? value.includes(item) : value === item}
+                    disabled={disabled}
+                    id={`${id}-${item}`}
+                    name={name}
+                    onChange={() => onChange(fieldType === Array ? xor(item, value) : item)}
+                    type="checkbox"
+                />
+
+                <label htmlFor={`${id}-${item}`}>
+                    {transform ? transform(item) : item}
+                </label>
+            </section>
+        </section>
+    )
+;
+
+const renderSelect = ({allowedValues, disabled, id, name, onChange, placeholder, transform, value}) =>
+    <select
+        disabled={disabled}
+        id={id}
+        name={name}
+        onChange={event => onChange(event.target.value)}
+        value={value}
+    >
+        {placeholder && (
+            <option value="" disabled hidden>
+                {placeholder}
+            </option>
+        )}
+
+        {allowedValues.map(value =>
+            <option key={value} value={value}>
+                {transform ? transform(value) : value}
+            </option>
+        )}
+    </select>
+;
+
 const Select = ({
     allowedValues,
+    checkboxes,
+    className,
     disabled,
     error,
+    fieldType,
     id,
     label,
     name,
@@ -16,33 +71,17 @@ const Select = ({
     value,
     ...props
 }) =>
-    <section className={classnames({disabled, error, required}, 'field')}>
+    <section className={classnames({disabled, error, required}, className, 'field')} {...props}>
         {label && (
             <label htmlFor={id}>
                 {label}
             </label>
         )}
 
-        <select
-            disabled={disabled}
-            id={id}
-            name={name}
-            onChange={event => onChange(event.target.value)}
-            value={value}
-            {...props}
-        >
-            {placeholder && (
-                <option value="" disabled hidden>
-                    {placeholder}
-                </option>
-            )}
-
-            {allowedValues.map(value =>
-                <option key={value} value={value}>
-                    {transform ? transform(value) : value}
-                </option>
-            )}
-        </select>
+        {checkboxes || fieldType === Array
+            ? renderCheckboxes({allowedValues, disabled, id, name, onChange, transform, value, fieldType})
+            : renderSelect    ({allowedValues, disabled, id, name, onChange, transform, value, placeholder})
+        }
     </section>
 ;
 
