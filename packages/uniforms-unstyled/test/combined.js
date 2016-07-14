789e0e4facdcd05f89353113e16701a8657857c6
Radosław Miernik <radekmie@gmail.com>
2016-07-14 23:30:35 +0200

    Fixed tests.

--- packages/uniforms-unstyled/test/combined.js
+++ packages/uniforms-unstyled/test/combined.js
@@ -265,7 +265,7 @@ describe('Everything', () => {
         wrapper.setProps({error: {}});
         wrapper.setProps({model: {x09: ['', '', '']}});
 
-        schema.x34 = {__type__: () => {}};
+        schema.x00 = {__type__: () => {}};
 
         expect(() => wrapper.update()).to.throw(/Unsupported field type/);
     });
