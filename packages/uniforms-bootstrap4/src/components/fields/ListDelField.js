6443e4ddefd9ca9b1437bb2ed7fea9238b68dafe
Radosław Miernik <radekmie@gmail.com>
2016-07-19 20:53:42 +0200

    Removed unwanted props from uniforms-bootstrap4 (#64).

--- packages/uniforms-bootstrap4/src/components/fields/ListDelField.js
+++ packages/uniforms-bootstrap4/src/components/fields/ListDelField.js
@@ -1,32 +1,17 @@
-import React          from 'react';
-import classnames     from 'classnames';
-import {connectField} from 'uniforms';
+import React            from 'react';
+import classnames       from 'classnames';
+import {connectField}   from 'uniforms';
+import {filterDOMProps} from 'uniforms';
 
 const ListDel = ({
-    changed,      // eslint-disable-line no-unused-vars
-    changedMap,   // eslint-disable-line no-unused-vars
     className,
-    disabled,     // eslint-disable-line no-unused-vars
-    error,        // eslint-disable-line no-unused-vars
-    errorMessage, // eslint-disable-line no-unused-vars
-    field,        // eslint-disable-line no-unused-vars
-    fieldType,    // eslint-disable-line no-unused-vars
-    fields,       // eslint-disable-line no-unused-vars
-    findError,    // eslint-disable-line no-unused-vars
-    findField,    // eslint-disable-line no-unused-vars
-    findValue,    // eslint-disable-line no-unused-vars
-    grid,         // eslint-disable-line no-unused-vars
-    id,           // eslint-disable-line no-unused-vars
-    label,        // eslint-disable-line no-unused-vars
+    disabled,
     name,
-    onChange,     // eslint-disable-line no-unused-vars
     parent,
-    placeholder,  // eslint-disable-line no-unused-vars
-    value,        // eslint-disable-line no-unused-vars
     ...props
 }) => {
     const fieldIndex      = +name.slice(1 + name.lastIndexOf('.'));
-    const limitNotReached = !(parent.minCount >= parent.value.length);
+    const limitNotReached = !disabled && !(parent.minCount >= parent.value.length);
 
     return (
         <span
@@ -35,7 +20,7 @@ const ListDel = ({
                 [].concat(parent.value.slice(0,  fieldIndex))
                   .concat(parent.value.slice(1 + fieldIndex))
             )}
-            {...props}
+            {...filterDOMProps(props)}
         >
             {/* TODO: configure to alternate icon */}
             <i className="octicon octicon-dash" />
--- packages/uniforms-bootstrap4/src/components/fields/NestField.js
+++ packages/uniforms-bootstrap4/src/components/fields/NestField.js
@@ -1,34 +1,19 @@
-import React          from 'react';
-import {connectField} from 'uniforms';
-import {injectName}   from 'uniforms';
-import {joinName}     from 'uniforms';
+import React            from 'react';
+import {connectField}   from 'uniforms';
+import {filterDOMProps} from 'uniforms';
+import {injectName}     from 'uniforms';
+import {joinName}       from 'uniforms';
 
 import AutoField from './AutoField';
 
 const Nest = ({
-    changed,      // eslint-disable-line no-unused-vars
-    changedMap,   // eslint-disable-line no-unused-vars
     children,
-    disabled,     // eslint-disable-line no-unused-vars
-    error,        // eslint-disable-line no-unused-vars
-    errorMessage, // eslint-disable-line no-unused-vars
-    field,        // eslint-disable-line no-unused-vars
-    fieldType,    // eslint-disable-line no-unused-vars
     fields,
-    findError,    // eslint-disable-line no-unused-vars
-    findField,    // eslint-disable-line no-unused-vars
-    findValue,    // eslint-disable-line no-unused-vars
-    grid,         // eslint-disable-line no-unused-vars
-    id,           // eslint-disable-line no-unused-vars
     label,
     name,
-    onChange,     // eslint-disable-line no-unused-vars
-    parent,       // eslint-disable-line no-unused-vars
-    placeholder,  // eslint-disable-line no-unused-vars
-    value,        // eslint-disable-line no-unused-vars
     ...props
 }) =>
-    <section {...props}>
+    <section {...filterDOMProps(props)}>
         {label && (
             <label>
                 {label}
