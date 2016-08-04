c0436bd7f2ddd4deb56ab477c4b291baeac1f6fc
alan bount <alan@zeroasterisk.com>
2016-08-04 10:21:14 -0400

    Removed `...props` from SubmitField section
    > Warning: Unknown prop `grid` on <section> tag.
    > Remove this prop from the element

--- packages/uniforms-bootstrap3/src/components/fields/SubmitField.js
+++ packages/uniforms-bootstrap3/src/components/fields/SubmitField.js
@@ -12,7 +12,7 @@ const SubmitField = ({
     wrapClassName,
     ...props
 }, {uniforms: {error, state: {disabled, grid}}}) =>
-    <section className={classnames(className, {'has-danger': error, row: grid})} {...props}>
+    <section className={classnames(className, {'has-danger': error, row: grid})}>
         {(grid || wrapClassName) && (
             <label className={classnames('form-control-label', gridClassName(grid, 'label'))}>
                 &nbsp;
