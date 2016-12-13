434a22b9bbedc93e8ecc100a814c0ddb06236528
Jan <jan.owsiany@vazco.eu>
2016-12-13 23:41:10 +0100

    DateField #WIP

--- packages/uniforms-material/src/components/fields/DateField.js
+++ packages/uniforms-material/src/components/fields/DateField.js
@@ -1,38 +1,64 @@
-import DatePicker       from 'material-ui/DatePicker';
-import React            from 'react';
-import {connectField}   from 'uniforms';
-import {filterDOMProps} from 'uniforms';
+import DatePicker         from 'material-ui/DatePicker';
+import React, {Component} from 'react';
+import TextField          from './TextField';
+import TimePicker         from 'material-ui/TimePicker';
+import {connectField}     from 'uniforms';
+import {ListItem}         from 'material-ui/List';
 
-const Date_ = ({
-    disabled,
-    errorMessage,
-    id,
-    inputRef,
-    label,
-    max,
-    min,
-    name,
-    onChange,
-    placeholder,
-    value,
-    ...props
-}) =>
-    <DatePicker
-        disabled={disabled}
-        errorText={errorMessage}
-        floatingLabelText={label}
-        hintText={placeholder}
-        id={id}
-        maxDate={max}
-        minDate={min}
-        name={name}
-        onChange={(event, date) => onChange(date)}
-        ref={inputRef}
-        value={value}
-        {...filterDOMProps(props)}
-    />
-;
+class Date_ extends Component {
+    render () {
+        const {
+            props: {
+                id,
+                max,
+                min,
+                onChange,
+                value,
+                ...props
+            }
+        } = this;
+
+        return (
+            <ListItem
+                disabled
+                primaryText={<div>
+                    <TextField
+                        onChange={onChange}
+                        onFocus={() => this.refs.datepicker.openDialog()}
+                        value={value}
+                        type="datetime"
+                        {...props}
+                    />
+                    <DatePicker
+                        value={value}
+                        maxDate={max}
+                        minDate={min}
+                        onChange={(event, date) => {
+                            if (value) {
+                                date.setHours(value.getHours());
+                                date.setMinutes(value.getMinutes());
+                            }
+                            onChange(date);
+                            // TODO: Risky race?
+                            this.refs.timepicker.openDialog();
+                        }}
+                        ref="datepicker"
+                        id={`${id}-date`}
+                        textFieldStyle={{display: 'none'}}
+                    />
+                    <TimePicker
+                        value={value}
+                        onChange={(event, date) => onChange(date)}
+                        ref="timepicker"
+                        id={`${id}-time`}
+                        textFieldStyle={{display: 'none'}}
+                    />
+                </div>}
+            />
+        );
+    }
+}
 
 Date_.displayName = 'Date';
 
-export default connectField(Date_, {ensureValue: false});
+export default connectField(Date_, {ensureValue: false, includeInChain: false});
