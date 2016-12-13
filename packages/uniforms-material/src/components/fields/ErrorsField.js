9391a87b59147ab58e165c93ad4ee344bb29ed07
Jan <jan.owsiany@vazco.eu>
2016-12-13 23:42:25 +0100

    ErrorsField

--- packages/uniforms-material/src/components/fields/ErrorsField.js
+++ packages/uniforms-material/src/components/fields/ErrorsField.js
@@ -1,21 +1,21 @@
+import ErrorOutline     from 'material-ui/svg-icons/alert/error-outline';
 import React            from 'react';
 import {BaseField}      from 'uniforms';
 import {filterDOMProps} from 'uniforms';
+import {List, ListItem} from 'material-ui/List';
 import {nothing}        from 'uniforms';
 
 const ErrorsField = ({children, ...props}, {uniforms: {error, schema}}) =>
     (!error && !children) ? nothing : (
-        <section {...filterDOMProps(props)}>
-            {children}
-
-            <ul>
-                {schema.getErrorMessages(error).map((message, index) =>
-                    <li key={index}>
-                        {message}
-                    </li>
-                )}
-            </ul>
-        </section>
+        <List {...filterDOMProps(props)}>
+            {!! children && (
+                <ListItem primaryText={children} disabled />
+            )}
+            {/* TODO: Make leftIcon optional */}
+            {schema.getErrorMessages(error).map((message, index) =>
+                <ListItem key={index} disabled primaryText={message} leftIcon={<ErrorOutline />} />
+            )}
+        </List>
     )
 ;
 
