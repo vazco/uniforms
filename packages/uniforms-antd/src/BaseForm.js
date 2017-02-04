920761638035f217ee96353c75b79da134a0393a
gervwyk <gervwyk@gmail.com>
2017-02-04 14:24:14 +0200

    Add fieldData for app context - #137

--- packages/uniforms-antd/src/BaseForm.js
+++ packages/uniforms-antd/src/BaseForm.js
@@ -1,9 +1,22 @@
-import BaseForm from 'uniforms/BaseForm';
+import BaseForm     from 'uniforms/BaseForm';
+import {PropTypes}  from 'react';
 
 const AntD = parent => class extends parent {
     static AntD = AntD;
 
+    static propTypes = {
+            ...parent.propTypes,
+            fieldData: PropTypes.object
+    };
+
     static displayName = `AntD${parent.displayName}`;
+
+    getChildContextState () {
+        return {
+            ...super.getChildContextState(),
+            fieldData: this.props.fieldData
+        };
+    };
 };
 
 export default AntD(BaseForm);
