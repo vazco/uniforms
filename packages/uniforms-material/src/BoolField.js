10ef9ba2445f4d23051109ab0ac19ba48bc52038
David Pankros <dpankros@users.noreply.github.com>
2017-02-10 17:57:24 -0500

    Added support for appearance prop in BoolField in uniforms-material (#189).

--- packages/uniforms-material/src/BoolField.js
+++ packages/uniforms-material/src/BoolField.js
@@ -1,9 +1,11 @@
 import Checkbox       from 'material-ui/Checkbox';
 import React          from 'react';
+import Toggle         from 'material-ui/Toggle';
 import connectField   from 'uniforms/connectField';
 import filterDOMProps from 'uniforms/filterDOMProps';
 
 const Bool = ({
+    appearance = 'checkbox',
     disabled,
     id,
     inputRef,
@@ -13,16 +15,26 @@ const Bool = ({
     value,
     ...props
 }) =>
-    <Checkbox
-        checked={!!value}
-        disabled={disabled}
-        id={id}
-        label={label}
-        name={name}
-        onCheck={(event, value) => disabled || onChange(value)}
-        ref={inputRef}
-        {...filterDOMProps(props)}
-    />
-;
+    appearance === 'toggle' ? (
+        <Toggle
+            toggled={!!value}
+            disabled={disabled}
+            id={id}
+            label={label}
+            name={name}
+            onToggle={(event, value) => disabled || onChange(value)}
+            ref={inputRef}{...filterDOMProps(props)}
+        />
+    ) : (
+        <Checkbox
+            checked={!!value}
+            disabled={disabled}
+            id={id}
+            label={label}
+            name={name}
+            onCheck={(event, value) => disabled || onChange(value)}
+            ref={inputRef}{...filterDOMProps(props)}
+        />
+    );
 
 export default connectField(Bool);
