e12427f9e1c8d46ebdf0a9f4ef84f4bbbc550450
Jan <jan.owsiany@vazco.eu>
2017-04-17 12:36:07 +0200

    uniforms joinName use jest instead of sinon and chai

--- packages/uniforms/__tests__/joinName.js
+++ packages/uniforms/__tests__/joinName.js
@@ -1,50 +1,48 @@
-import {expect} from 'chai';
-
 import joinName from 'uniforms/joinName';
 
 describe('joinName', () => {
     it('is a function', () => {
-        expect(joinName).to.be.a('function');
+        expect(joinName).toBeInstanceOf(Function);
     });
 
     it('have raw mode', () => {
-        expect(joinName(null))                   .to.deep.equal([]);
-        expect(joinName(null, 'a'))              .to.deep.equal(['a']);
-        expect(joinName(null, 'a', 'b'))         .to.deep.equal(['a', 'b']);
-        expect(joinName(null, 'a', 'b', null))   .to.deep.equal(['a', 'b']);
-        expect(joinName(null, 'a', 'b', null, 1)).to.deep.equal(['a', 'b', '1']);
+        expect(joinName(null))                   .toEqual([]);
+        expect(joinName(null, 'a'))              .toEqual(['a']);
+        expect(joinName(null, 'a', 'b'))         .toEqual(['a', 'b']);
+        expect(joinName(null, 'a', 'b', null))   .toEqual(['a', 'b']);
+        expect(joinName(null, 'a', 'b', null, 1)).toEqual(['a', 'b', '1']);
     });
 
     it('works with arrays', () => {
-        expect(joinName(['a'], 'b')).to.equal('a.b');
-        expect(joinName('a', ['b'])).to.equal('a.b');
+        expect(joinName(['a'], 'b')).toEqual('a.b');
+        expect(joinName('a', ['b'])).toEqual('a.b');
     });
 
     it('works with empty strings', () => {
-        expect(joinName('', 'a', 'b')).to.equal('a.b');
-        expect(joinName('a', '', 'b')).to.equal('a.b');
-        expect(joinName('a', 'b', '')).to.equal('a.b');
+        expect(joinName('', 'a', 'b')).toEqual('a.b');
+        expect(joinName('a', '', 'b')).toEqual('a.b');
+        expect(joinName('a', 'b', '')).toEqual('a.b');
     });
 
     it('works with falsy values', () => {
-        expect(joinName('a', null,      'b')).to.equal('a.b');
-        expect(joinName('a', false,     'b')).to.equal('a.b');
-        expect(joinName('a', undefined, 'b')).to.equal('a.b');
+        expect(joinName('a', null,      'b')).toEqual('a.b');
+        expect(joinName('a', false,     'b')).toEqual('a.b');
+        expect(joinName('a', undefined, 'b')).toEqual('a.b');
     });
 
     it('works with numbers', () => {
-        expect(joinName(1, 'a', 'b')).to.equal('1.a.b');
-        expect(joinName('a', 1, 'b')).to.equal('a.1.b');
-        expect(joinName('a', 'b', 1)).to.equal('a.b.1');
+        expect(joinName(1, 'a', 'b')).toEqual('1.a.b');
+        expect(joinName('a', 1, 'b')).toEqual('a.1.b');
+        expect(joinName('a', 'b', 1)).toEqual('a.b.1');
     });
 
     it('works with partials', () => {
-        expect(joinName('a', 'b.c.d')).to.equal('a.b.c.d');
-        expect(joinName('a.b', 'c.d')).to.equal('a.b.c.d');
-        expect(joinName('a.b.c', 'd')).to.equal('a.b.c.d');
+        expect(joinName('a', 'b.c.d')).toEqual('a.b.c.d');
+        expect(joinName('a.b', 'c.d')).toEqual('a.b.c.d');
+        expect(joinName('a.b.c', 'd')).toEqual('a.b.c.d');
 
-        expect(joinName(null, 'a', 'b.c.d')).to.deep.equal(['a', 'b', 'c', 'd']);
-        expect(joinName(null, 'a.b', 'c.d')).to.deep.equal(['a', 'b', 'c', 'd']);
-        expect(joinName(null, 'a.b.c', 'd')).to.deep.equal(['a', 'b', 'c', 'd']);
+        expect(joinName(null, 'a', 'b.c.d')).toEqual(['a', 'b', 'c', 'd']);
+        expect(joinName(null, 'a.b', 'c.d')).toEqual(['a', 'b', 'c', 'd']);
+        expect(joinName(null, 'a.b.c', 'd')).toEqual(['a', 'b', 'c', 'd']);
     });
 });
