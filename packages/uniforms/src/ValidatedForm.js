4287c5c882d2c8d7605e8a02fec46f273a804f74
Radosław Miernik <radekmie@gmail.com>
2017-06-09 07:23:59 +0200

    Reverted #293.

--- packages/uniforms/src/ValidatedForm.js
+++ packages/uniforms/src/ValidatedForm.js
@@ -103,9 +103,10 @@ const Validated = parent => class extends parent {
         // eslint-disable-next-line max-len
         if (this.props.validate === 'onChange' || this.props.validate === 'onChangeAfterSubmit' && this.state.validate) {
             this.onValidate(key, value);
-        } else if (this.props.validate === 'onSubmit' && this.state.validate) {
-            this.setState(() => ({error: null}));
         }
+        // FIXME: https://github.com/vazco/uniforms/issues/293
+        // if (this.props.validate === 'onSubmit' && this.state.validate) {
+        //     this.setState(() => ({error: null}));
 
         super.onChange(...arguments);
     }
