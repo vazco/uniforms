4ed532595449289376bbb907854f311314d09fec
Radosław Miernik <radekmie@gmail.com>
2017-06-26 19:14:03 +0200

    Separated uniforms-bootstrap3 components tests.

---
+++ packages/uniforms-bootstrap3/__tests__/FormGroup.js
@@ -0,0 +1,12 @@
+import React   from 'react';
+import {mount} from 'enzyme';
+
+import FormGroup from 'uniforms-bootstrap3/FormGroup';
+
+test('FormGroup should be deprecated', () => {
+    console.error = jest.fn();
+
+    mount(<FormGroup />);
+
+    expect(console.error).toHaveBeenCalledTimes(1);
+});
---
+++ packages/uniforms-bootstrap3/__tests__/ValidatedForm.js
@@ -0,0 +1,13 @@
+import React   from 'react';
+import {mount} from 'enzyme';
+
+import ValidatedForm from 'uniforms-bootstrap3/ValidatedForm';
+
+import createSchema from './_createSchema';
+
+test('<ValidatedForm> - works', () => {
+    const element = <ValidatedForm schema={createSchema()} />;
+    const wrapper = mount(element);
+
+    expect(wrapper.find(ValidatedForm)).toHaveLength(1);
+});
