a8e026ef7d1b02fdd1d216509f5394afbe37a2db
Radosław Miernik <radekmie@gmail.com>
2017-06-26 19:14:14 +0200

    Separated uniforms-bootstrap4 components tests.

---
+++ packages/uniforms-bootstrap4/__tests__/QuickForm.js
@@ -0,0 +1,13 @@
+import React   from 'react';
+import {mount} from 'enzyme';
+
+import QuickForm from 'uniforms-bootstrap4/QuickForm';
+
+import createSchema from './_createSchema';
+
+test('<QuickForm> - renders', () => {
+    const element = <QuickForm schema={createSchema()} />;
+    const wrapper = mount(element);
+
+    expect(wrapper).toHaveLength(1);
+});
