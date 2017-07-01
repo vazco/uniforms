b594b4a46a3979c17a5835d46cdf0c0757494773
Jan Owsiany <jan.owsiany@gmail.com>
2017-07-01 17:57:31 +0200

    Make uniforms-material TextField tests pass

--- packages/uniforms-material/__tests__/TextField.js
+++ packages/uniforms-material/__tests__/TextField.js
@@ -1,118 +1,112 @@
 import React   from 'react';
 import {mount} from 'enzyme';
 
+import TextFieldMaterial from 'material-ui/TextField';
+
 import TextField from 'uniforms-material/TextField';
 
 import createContext from './_createContext';
 
-test('<TextField> - renders an input', () => {
+test('<TextField> - renders an TextField', () => {
     const element = <TextField name="x" />;
     const wrapper = mount(element, createContext({x: {type: String}}));
 
-    expect(wrapper.find('input')).toHaveLength(1);
+    expect(wrapper.find(TextFieldMaterial)).toHaveLength(1);
 });
 
-test('<TextField> - renders an input with correct disabled state', () => {
+test('<TextField> - renders a TextField with correct disabled state', () => {
     const element = <TextField name="x" disabled />;
     const wrapper = mount(element, createContext({x: {type: String}}));
 
-    expect(wrapper.find('input')).toHaveLength(1);
-    expect(wrapper.find('input').prop('disabled')).toBe(true);
+    expect(wrapper.find(TextFieldMaterial)).toHaveLength(1);
+    expect(wrapper.find(TextFieldMaterial).prop('disabled')).toBe(true);
 });
 
-test('<TextField> - renders an input with correct id (inherited)', () => {
+test('<TextField> - renders a TextField with correct id (inherited)', () => {
     const element = <TextField name="x" />;
     const wrapper = mount(element, createContext({x: {type: String}}));
 
-    expect(wrapper.find('input')).toHaveLength(1);
-    expect(wrapper.find('input').prop('id')).toBeTruthy();
+    expect(wrapper.find(TextFieldMaterial)).toHaveLength(1);
+    expect(wrapper.find(TextFieldMaterial).prop('id')).toBeTruthy();
 });
 
-test('<TextField> - renders an input with correct id (specified)', () => {
+test('<TextField> - renders a TextField with correct id (specified)', () => {
     const element = <TextField name="x" id="y" />;
     const wrapper = mount(element, createContext({x: {type: String}}));
 
-    expect(wrapper.find('input')).toHaveLength(1);
-    expect(wrapper.find('input').prop('id')).toBe('y');
+    expect(wrapper.find(TextFieldMaterial)).toHaveLength(1);
+    expect(wrapper.find(TextFieldMaterial).prop('id')).toBe('y');
 });
 
-test('<TextField> - renders an input with correct name', () => {
+test('<TextField> - renders a TextField with correct name', () => {
     const element = <TextField name="x" />;
     const wrapper = mount(element, createContext({x: {type: String}}));
 
-    expect(wrapper.find('input')).toHaveLength(1);
-    expect(wrapper.find('input').prop('name')).toBe('x');
+    expect(wrapper.find(TextFieldMaterial)).toHaveLength(1);
+    expect(wrapper.find(TextFieldMaterial).prop('name')).toBe('x');
 });
 
-test('<TextField> - renders an input with correct placeholder', () => {
+test('<TextField> - renders a TextField with correct hintText', () => {
     const element = <TextField name="x" placeholder="y" />;
     const wrapper = mount(element, createContext({x: {type: String}}));
 
-    expect(wrapper.find('input')).toHaveLength(1);
-    expect(wrapper.find('input').prop('placeholder')).toBe('y');
+    expect(wrapper.find(TextFieldMaterial)).toHaveLength(1);
+    expect(wrapper.find(TextFieldMaterial).prop('hintText')).toBe('y');
 });
 
-test('<TextField> - renders an input with correct type', () => {
+test('<TextField> - renders a TextField with correct value (default)', () => {
     const element = <TextField name="x" />;
     const wrapper = mount(element, createContext({x: {type: String}}));
 
-    expect(wrapper.find('input')).toHaveLength(1);
-    expect(wrapper.find('input').prop('type')).toBe('text');
+    expect(wrapper.find(TextFieldMaterial)).toHaveLength(1);
+    expect(wrapper.find(TextFieldMaterial).prop('value')).toBe('');
 });
 
-test('<TextField> - renders an input with correct value (default)', () => {
-    const element = <TextField name="x" />;
-    const wrapper = mount(element, createContext({x: {type: String}}));
-
-    expect(wrapper.find('input')).toHaveLength(1);
-    expect(wrapper.find('input').prop('value')).toBe('');
-});
-
-test('<TextField> - renders an input with correct value (model)', () => {
+test('<TextField> - renders a TextField with correct value (model)', () => {
     const element = <TextField name="x" />;
     const wrapper = mount(element, createContext({x: {type: String}}, {model: {x: 'y'}}));
 
-    expect(wrapper.find('input')).toHaveLength(1);
-    expect(wrapper.find('input').prop('value')).toBe('y');
+    expect(wrapper.find(TextFieldMaterial)).toHaveLength(1);
+    expect(wrapper.find(TextFieldMaterial).prop('value')).toBe('y');
 });
 
-test('<TextField> - renders an input with correct value (specified)', () => {
+test('<TextField> - renders a TextField with correct value (specified)', () => {
     const element = <TextField name="x" value="y" />;
     const wrapper = mount(element, createContext({x: {type: String}}));
 
-    expect(wrapper.find('input')).toHaveLength(1);
-    expect(wrapper.find('input').prop('value')).toBe('y');
+    expect(wrapper.find(TextFieldMaterial)).toHaveLength(1);
+    expect(wrapper.find(TextFieldMaterial).prop('value')).toBe('y');
 });
 
-test('<TextField> - renders an input which correctly reacts on change', () => {
+test('<TextField> - renders a TextField which correctly reacts on change', () => {
     const onChange = jest.fn();
 
     const element = <TextField name="x" />;
     const wrapper = mount(element, createContext({x: {type: String}}, {onChange}));
 
-    expect(wrapper.find('input')).toHaveLength(1);
+    expect(wrapper.find(TextFieldMaterial)).toHaveLength(1);
     expect(wrapper.find('input').simulate('change', {target: {value: 'y'}})).toBeTruthy();
     expect(onChange).toHaveBeenLastCalledWith('x', 'y');
 });
 
-test('<TextField> - renders an input which correctly reacts on change (empty)', () => {
+test('<TextField> - renders a TextField which correctly reacts on change (empty)', () => {
     const onChange = jest.fn();
 
     const element = <TextField name="x" />;
     const wrapper = mount(element, createContext({x: {type: String}}, {onChange}));
 
-    expect(wrapper.find('input')).toHaveLength(1);
+    expect(wrapper.find(TextFieldMaterial)).toHaveLength(1);
     expect(wrapper.find('input').simulate('change', {target: {value: ''}})).toBeTruthy();
     expect(onChange).toHaveBeenLastCalledWith('x', '');
 });
 
-test('<TextField> - renders an input which correctly reacts on change (same value)', () => {
+test('<TextField> - renders a TextField which correctly reacts on change (same value)', () => {
     const onChange = jest.fn();
 
     const element = <TextField name="x" />;
     const wrapper = mount(element, createContext({x: {type: String}}, {model: {x: 'y'}, onChange}));
 
-    expect(wrapper.find('input')).toHaveLength(1);
+    expect(wrapper.find(TextFieldMaterial)).toHaveLength(1);
     expect(wrapper.find('input').simulate('change', {target: {value: 'y'}})).toBeTruthy();
     expect(onChange).toHaveBeenLastCalledWith('x', 'y');
 });
@@ -121,7 +115,6 @@ test('<TextField> - renders a label', () => {
     const element = <TextField name="x" label="y" />;
     const wrapper = mount(element, createContext({x: {type: String}}));
 
-    expect(wrapper.find('label')).toHaveLength(1);
-    expect(wrapper.find('label').text()).toBe('y');
-    expect(wrapper.find('label').prop('htmlFor')).toBe(wrapper.find('input').prop('id'));
+    expect(wrapper.find(TextFieldMaterial)).toHaveLength(1);
+    expect(wrapper.find(TextFieldMaterial).prop('floatingLabelText')).toBe('y');
 });
