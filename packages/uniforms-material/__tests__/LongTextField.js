14fb98b6a42041d85cb2536c02d6013b67d8b136
Jan Owsiany <jan.owsiany@gmail.com>
2017-07-01 18:00:40 +0200

    Make uniforms-material LongTextField tests pass

--- packages/uniforms-material/__tests__/LongTextField.js
+++ packages/uniforms-material/__tests__/LongTextField.js
@@ -1,19 +1,20 @@
 import React     from 'react';
 import {mount}   from 'enzyme';
+
 import TextField from 'material-ui/TextField';
 
 import LongTextField from 'uniforms-material/LongTextField';
 
 import createContext from './_createContext';
 
-test('<LongTextField> - renders a textarea', () => {
+test('<LongTextField> - renders a TextField', () => {
     const element = <LongTextField name="x" />;
     const wrapper = mount(element, createContext({x: {type: String}}));
 
     expect(wrapper.find(TextField)).toHaveLength(1);
 });
 
-test('<LongTextField> - renders a textarea with correct disabled state', () => {
+test('<LongTextField> - renders a TextField with correct disabled state', () => {
     const element = <LongTextField name="x" disabled />;
     const wrapper = mount(element, createContext({x: {type: String}}));
 
@@ -21,7 +22,7 @@ test('<LongTextField> - renders a textarea with correct disabled state', () => {
     expect(wrapper.find(TextField).prop('disabled')).toBe(true);
 });
 
-test('<LongTextField> - renders a textarea with correct id (inherited)', () => {
+test('<LongTextField> - renders a TextField with correct id (inherited)', () => {
     const element = <LongTextField name="x" />;
     const wrapper = mount(element, createContext({x: {type: String}}));
 
@@ -29,7 +30,7 @@ test('<LongTextField> - renders a textarea with correct id (inherited)', () => {
     expect(wrapper.find(TextField).prop('id')).toBeTruthy();
 });
 
-test('<LongTextField> - renders a textarea with correct id (specified)', () => {
+test('<LongTextField> - renders a TextField with correct id (specified)', () => {
     const element = <LongTextField name="x" id="y" />;
     const wrapper = mount(element, createContext({x: {type: String}}));
 
@@ -37,7 +38,7 @@ test('<LongTextField> - renders a textarea with correct id (specified)', () => {
     expect(wrapper.find(TextField).prop('id')).toBe('y');
 });
 
-test('<LongTextField> - renders a textarea with correct name', () => {
+test('<LongTextField> - renders a TextField with correct name', () => {
     const element = <LongTextField name="x" />;
     const wrapper = mount(element, createContext({x: {type: String}}));
 
@@ -45,15 +46,15 @@ test('<LongTextField> - renders a textarea with correct name', () => {
     expect(wrapper.find(TextField).prop('name')).toBe('x');
 });
 
-test('<LongTextField> - renders a textarea with correct placeholder', () => {
+test('<LongTextField> - renders a TextField with correct hintText', () => {
     const element = <LongTextField name="x" placeholder="y" />;
     const wrapper = mount(element, createContext({x: {type: String}}));
 
     expect(wrapper.find(TextField)).toHaveLength(1);
-    expect(wrapper.find(TextField).prop('placeholder')).toBe('y');
+    expect(wrapper.find(TextField).prop('hintText')).toBe('y');
 });
 
-test('<LongTextField> - renders a textarea with correct value (default)', () => {
+test('<LongTextField> - renders a TextField with correct value (default)', () => {
     const element = <LongTextField name="x" />;
     const wrapper = mount(element, createContext({x: {type: String}}));
 
@@ -61,7 +62,7 @@ test('<LongTextField> - renders a textarea with correct value (default)', () => 
     expect(wrapper.find(TextField).prop('value')).toBe('');
 });
 
-test('<LongTextField> - renders a textarea with correct value (model)', () => {
+test('<LongTextField> - renders a TextField with correct value (model)', () => {
     const element = <LongTextField name="x" />;
     const wrapper = mount(element, createContext({x: {type: String}}, {model: {x: 'y'}}));
 
@@ -69,7 +70,7 @@ test('<LongTextField> - renders a textarea with correct value (model)', () => {
     expect(wrapper.find(TextField).prop('value')).toBe('y');
 });
 
-test('<LongTextField> - renders a textarea with correct value (specified)', () => {
+test('<LongTextField> - renders a TextField with correct value (specified)', () => {
     const element = <LongTextField name="x" value="y" />;
     const wrapper = mount(element, createContext({x: {type: String}}));
 
@@ -77,36 +78,36 @@ test('<LongTextField> - renders a textarea with correct value (specified)', () =
     expect(wrapper.find(TextField).prop('value')).toBe('y');
 });
 
-test('<LongTextField> - renders a textarea which correctly reacts on change', () => {
+test('<LongTextField> - renders a TextField which correctly reacts on change', () => {
     const onChange = jest.fn();
 
     const element = <LongTextField name="x" />;
     const wrapper = mount(element, createContext({x: {type: String}}, {onChange}));
 
     expect(wrapper.find(TextField)).toHaveLength(1);
-    expect(wrapper.find(TextField).simulate('change', {target: {value: 'y'}})).toBeTruthy();
+    expect(wrapper.find('input').simulate('change', {target: {value: 'y'}})).toBeTruthy();
     expect(onChange).toHaveBeenLastCalledWith('x', 'y');
 });
 
-test('<LongTextField> - renders a textarea which correctly reacts on change (empty)', () => {
+test('<LongTextField> - renders a TextField which correctly reacts on change (empty)', () => {
     const onChange = jest.fn();
 
     const element = <LongTextField name="x" />;
     const wrapper = mount(element, createContext({x: {type: String}}, {onChange}));
 
     expect(wrapper.find(TextField)).toHaveLength(1);
-    expect(wrapper.find(TextField).simulate('change', {target: {value: ''}})).toBeTruthy();
+    expect(wrapper.find('input').simulate('change', {target: {value: ''}})).toBeTruthy();
     expect(onChange).toHaveBeenLastCalledWith('x', '');
 });
 
-test('<LongTextField> - renders a textarea which correctly reacts on change (same value)', () => {
+test('<LongTextField> - renders a TextField which correctly reacts on change (same value)', () => {
     const onChange = jest.fn();
 
     const element = <LongTextField name="x" />;
     const wrapper = mount(element, createContext({x: {type: String}}, {model: {x: 'y'}, onChange}));
 
     expect(wrapper.find(TextField)).toHaveLength(1);
-    expect(wrapper.find(TextField).simulate('change', {target: {value: 'y'}})).toBeTruthy();
+    expect(wrapper.find('input').simulate('change', {target: {value: 'y'}})).toBeTruthy();
     expect(onChange).toHaveBeenLastCalledWith('x', 'y');
 });
 
