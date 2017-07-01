b2ac21ff743c57571f841628e4292e486a491079
Jan Owsiany <jan.owsiany@gmail.com>
2017-07-01 20:02:00 +0200

    Make uniforms-material SelectField tests pass

--- packages/uniforms-material/__tests__/SelectField.js
+++ packages/uniforms-material/__tests__/SelectField.js
@@ -5,21 +5,19 @@ import Subheader           from 'material-ui/Subheader';
 import SelectFieldMaterial from 'material-ui/SelectField';
 import CheckboxMaterial    from 'material-ui/Checkbox';
 import RadioButtonMaterial from 'material-ui/RadioButton';
-// import MenuItem            from 'material-ui/MenuItem';
 
 import SelectField from 'uniforms-material/SelectField';
 
 import createContext from './_createContext';
-import touchTap from './_touchTap';
 
-test('<SelectField> - renders a select', () => {
+test('<SelectField> - renders a SelectField', () => {
     const element = <SelectField name="x" />;
     const wrapper = mount(element, createContext({x: {type: String, allowedValues: ['a', 'b']}}));
 
     expect(wrapper.find(SelectFieldMaterial)).toHaveLength(1);
 });
 
-test('<SelectField> - renders a select with correct disabled state', () => {
+test('<SelectField> - renders a SelectField with correct disabled state', () => {
     const element = <SelectField name="x" disabled />;
     const wrapper = mount(element, createContext({x: {type: String, allowedValues: ['a', 'b']}}));
 
@@ -27,7 +25,7 @@ test('<SelectField> - renders a select with correct disabled state', () => {
     expect(wrapper.find(SelectFieldMaterial).prop('disabled')).toBe(true);
 });
 
-test('<SelectField> - renders a select with correct id (inherited)', () => {
+test('<SelectField> - renders a SelectField with correct id (inherited)', () => {
     const element = <SelectField name="x" />;
     const wrapper = mount(element, createContext({x: {type: String, allowedValues: ['a', 'b']}}));
 
@@ -35,7 +33,7 @@ test('<SelectField> - renders a select with correct id (inherited)', () => {
     expect(wrapper.find(SelectFieldMaterial).prop('id')).toBeTruthy();
 });
 
-test('<SelectField> - renders a select with correct id (specified)', () => {
+test('<SelectField> - renders a SelectField with correct id (specified)', () => {
     const element = <SelectField name="x" id="y" />;
     const wrapper = mount(element, createContext({x: {type: String, allowedValues: ['a', 'b']}}));
 
@@ -43,7 +41,7 @@ test('<SelectField> - renders a select with correct id (specified)', () => {
     expect(wrapper.find(SelectFieldMaterial).prop('id')).toBe('y');
 });
 
-test('<SelectField> - renders a select with correct name', () => {
+test('<SelectField> - renders a SelectField with correct name', () => {
     const element = <SelectField name="x" />;
     const wrapper = mount(element, createContext({x: {type: String, allowedValues: ['a', 'b']}}));
 
@@ -51,42 +49,7 @@ test('<SelectField> - renders a select with correct name', () => {
     expect(wrapper.find(SelectFieldMaterial).prop('name')).toBe('x');
 });
 
-// TODO: Remove this ?
-// test('<SelectField> - renders a select with correct options', () => {
-//     const element = <SelectField name="x" />;
-//     const wrapper = mount(element, createContext({x: {type: String, allowedValues: ['a', 'b']}}));
-
-//     expect(wrapper.find(SelectFieldMaterial)).toHaveLength(1);
-//     expect(wrapper.find(SelectFieldMaterial).find(MenuItem)).toHaveLength(2);
-//     expect(wrapper.find(SelectFieldMaterial).find(MenuItem).at(0).prop('value')).toBe('a');
-//     expect(wrapper.find(SelectFieldMaterial).find(MenuItem).at(0).text()).toBe('a');
-//     expect(wrapper.find(SelectFieldMaterial).find(MenuItem).at(1).prop('value')).toBe('b');
-//     expect(wrapper.find(SelectFieldMaterial).find(MenuItem).at(1).text()).toBe('b');
-// });
-
-// TODO: Remove this ?
-// test('<SelectField> - renders a select with correct options (transform)', () => {
-//     const element = <SelectField name="x" transform={x => x.toUpperCase()} />;
-//     const wrapper = mount(element, createContext({x: {type: String, allowedValues: ['a', 'b']}}));
-
-//     expect(wrapper.find(SelectFieldMaterial)).toHaveLength(1);
-//     expect(wrapper.find(SelectFieldMaterial).find(MenuItem)).toHaveLength(2);
-//     expect(wrapper.find(SelectFieldMaterial).find(MenuItem).at(0).prop('value')).toBe('a');
-//     expect(wrapper.find(SelectFieldMaterial).find(MenuItem).at(0).text()).toBe('A');
-//     expect(wrapper.find(SelectFieldMaterial).find(MenuItem).at(1).prop('value')).toBe('b');
-//     expect(wrapper.find(SelectFieldMaterial).find(MenuItem).at(1).text()).toBe('B');
-// });
-
-// TODO: Remove this ?
-// test('<SelectField> - renders a select with correct placeholder (fallback)', () => {
-//     const element = <SelectField name="x" label="y" placeholder="" />;
-//     const wrapper = mount(element, createContext({x: {type: String, allowedValues: ['a', 'b'], optional: true}}));
-
-//     expect(wrapper.find(SelectFieldMaterial)).toHaveLength(1);
-//     expect(wrapper.find(SelectFieldMaterial).prop('hintText')).toBe('y');
-// });
-
-test('<SelectField> - renders a select with correct placeholder (implicit)', () => {
+test('<SelectField> - renders a SelectField with correct placeholder (implicit)', () => {
     const element = <SelectField name="x" placeholder="y" />;
     const wrapper = mount(element, createContext({x: {type: String, allowedValues: ['a', 'b']}}));
 
@@ -94,15 +57,15 @@ test('<SelectField> - renders a select with correct placeholder (implicit)', () 
     expect(wrapper.find(SelectFieldMaterial).prop('hintText')).toBe('y');
 });
 
-test('<SelectField> - renders a select with correct value (default)', () => {
+test('<SelectField> - renders a SelectField with correct value (default)', () => {
     const element = <SelectField name="x" />;
     const wrapper = mount(element, createContext({x: {type: String, allowedValues: ['a', 'b']}}));
 
     expect(wrapper.find(SelectFieldMaterial)).toHaveLength(1);
-    expect(wrapper.find(SelectFieldMaterial).prop('value')).toBeUndefined(); // TODO: Fix ?
+    expect(wrapper.find(SelectFieldMaterial).prop('value')).toBeUndefined();
 });
 
-test('<SelectField> - renders a select with correct value (model)', () => {
+test('<SelectField> - renders a SelectField with correct value (model)', () => {
     const element = <SelectField name="x" />;
     const wrapper = mount(element, createContext({x: {type: String, allowedValues: ['a', 'b']}}, {model: {x: 'b'}}));
 
@@ -110,7 +73,7 @@ test('<SelectField> - renders a select with correct value (model)', () => {
     expect(wrapper.find(SelectFieldMaterial).prop('value')).toBe('b');
 });
 
-test('<SelectField> - renders a select with correct value (specified)', () => {
+test('<SelectField> - renders a SelectField with correct value (specified)', () => {
     const element = <SelectField name="x" value="b" />;
     const wrapper = mount(element, createContext({x: {type: String, allowedValues: ['a', 'b']}}));
 
@@ -118,7 +81,7 @@ test('<SelectField> - renders a select with correct value (specified)', () => {
     expect(wrapper.find(SelectFieldMaterial).prop('value')).toBe('b');
 });
 
-test('<SelectField> - renders a select which correctly reacts on change', () => {
+test('<SelectField> - renders a SelectField which correctly reacts on change', () => {
     const onChange = jest.fn();
 
     const element = <SelectField name="x" />;
@@ -129,7 +92,7 @@ test('<SelectField> - renders a select which correctly reacts on change', () => 
     expect(onChange).toHaveBeenLastCalledWith('x', 'b');
 });
 
-test('<SelectField> - renders a select which correctly reacts on change (empty)', () => {
+test('<SelectField> - renders a SelectField which correctly reacts on change (empty)', () => {
     const onChange = jest.fn();
 
     const element = <SelectField name="x" />;
@@ -140,7 +103,7 @@ test('<SelectField> - renders a select which correctly reacts on change (empty)'
     expect(onChange).toHaveBeenLastCalledWith('x', '');
 });
 
-test('<SelectField> - renders a select which correctly reacts on change (same value)', () => {
+test('<SelectField> - renders a SelectField which correctly reacts on change (same value)', () => {
     const onChange = jest.fn();
 
     const element = <SelectField name="x" />;
@@ -159,50 +122,50 @@ test('<SelectField> - renders a label', () => {
     expect(wrapper.find(SelectFieldMaterial).prop('floatingLabelText')).toBe('y');
 });
 
-test('<SelectField checkboxes> - renders a set of checkboxes', () => {
+test('<SelectField checkboxes> - renders a set of RadioButtons', () => {
     const element = <SelectField checkboxes name="x" />;
     const wrapper = mount(element, createContext({x: {type: String, allowedValues: ['a', 'b']}}));
 
-    expect(wrapper.find('input')).toHaveLength(2);
+    expect(wrapper.find(RadioButtonMaterial)).toHaveLength(2);
 });
 
-test('<SelectField checkboxes> - renders a set of checkboxes with correct disabled state', () => {
+test('<SelectField checkboxes> - renders a set of RadioButtons with correct disabled state', () => {
     const element = <SelectField checkboxes name="x" disabled />;
     const wrapper = mount(element, createContext({x: {type: String, allowedValues: ['a', 'b']}}));
 
-    expect(wrapper.find('input')).toHaveLength(2);
-    expect(wrapper.find('input').at(0).prop('disabled')).toBe(true);
-    expect(wrapper.find('input').at(1).prop('disabled')).toBe(true);
+    expect(wrapper.find(RadioButtonMaterial)).toHaveLength(2);
+    expect(wrapper.find(RadioButtonMaterial).at(0).prop('disabled')).toBe(true);
+    expect(wrapper.find(RadioButtonMaterial).at(1).prop('disabled')).toBe(true);
 });
 
-test('<SelectField checkboxes> - renders a set of checkboxes with correct id (inherited)', () => {
+test('<SelectField checkboxes> - renders a set of RadioButtons with correct id (inherited)', () => {
     const element = <SelectField checkboxes name="x" />;
     const wrapper = mount(element, createContext({x: {type: String, allowedValues: ['a', 'b']}}));
 
-    expect(wrapper.find('input')).toHaveLength(2);
-    expect(wrapper.find('input').at(0).prop('id')).toBeTruthy();
-    expect(wrapper.find('input').at(1).prop('id')).toBeTruthy();
+    expect(wrapper.find(RadioButtonMaterial)).toHaveLength(2);
+    expect(wrapper.find(RadioButtonMaterial).at(0).prop('id')).toBeTruthy();
+    expect(wrapper.find(RadioButtonMaterial).at(1).prop('id')).toBeTruthy();
 });
 
-test('<SelectField checkboxes> - renders a set of checkboxes with correct id (specified)', () => {
+test('<SelectField checkboxes> - renders a set of RadioButtons with correct id (specified)', () => {
     const element = <SelectField checkboxes name="x" id="y" />;
     const wrapper = mount(element, createContext({x: {type: String, allowedValues: ['a', 'b']}}));
 
-    expect(wrapper.find('input')).toHaveLength(2);
-    expect(wrapper.find('input').at(0).prop('id')).toBe('y-a');
-    expect(wrapper.find('input').at(1).prop('id')).toBe('y-b');
+    expect(wrapper.find(RadioButtonMaterial)).toHaveLength(2);
+    expect(wrapper.find(RadioButtonMaterial).at(0).prop('id')).toBe('y-a');
+    expect(wrapper.find(RadioButtonMaterial).at(1).prop('id')).toBe('y-b');
 });
 
-test('<SelectField checkboxes> - renders a set of checkboxes with correct name', () => {
+test('<SelectField checkboxes> - renders a set of RadioButtons with correct name', () => {
     const element = <SelectField checkboxes name="x" />;
     const wrapper = mount(element, createContext({x: {type: String, allowedValues: ['a', 'b']}}));
 
-    expect(wrapper.find('input')).toHaveLength(2);
-    expect(wrapper.find('input').at(0).prop('name')).toBe('x');
-    expect(wrapper.find('input').at(1).prop('name')).toBe('x');
+    expect(wrapper.find(RadioButtonMaterial)).toHaveLength(2);
+    expect(wrapper.find(RadioButtonMaterial).at(0).prop('name')).toBe('x');
+    expect(wrapper.find(RadioButtonMaterial).at(1).prop('name')).toBe('x');
 });
 
-test('<SelectField checkboxes> - renders a set of checkboxes with correct options', () => {
+test('<SelectField checkboxes> - renders a set of RadioButtons with correct options', () => {
     const element = <SelectField checkboxes name="x" />;
     const wrapper = mount(element, createContext({x: {type: String, allowedValues: ['a', 'b']}}));
 
@@ -211,7 +174,7 @@ test('<SelectField checkboxes> - renders a set of checkboxes with correct option
     expect(wrapper.find(RadioButtonMaterial).at(1).prop('label')).toBe('b');
 });
 
-test('<SelectField checkboxes> - renders a set of checkboxes with correct options (transform)', () => {
+test('<SelectField checkboxes> - renders a set of RadioButtons with correct options (transform)', () => {
     const element = <SelectField checkboxes name="x" transform={x => x.toUpperCase()} />;
     const wrapper = mount(element, createContext({x: {type: String, allowedValues: ['a', 'b']}}));
 
@@ -220,7 +183,7 @@ test('<SelectField checkboxes> - renders a set of checkboxes with correct option
     expect(wrapper.find(RadioButtonMaterial).at(1).prop('label')).toBe('B');
 });
 
-test('<SelectField checkboxes> - renders a set of checkboxes with correct value (default)', () => {
+test('<SelectField checkboxes> - renders a set of RadioButtons with correct value (default)', () => {
     const element = <SelectField checkboxes name="x" />;
     const wrapper = mount(element, createContext({x: {type: String, allowedValues: ['a', 'b']}}));
 
@@ -229,7 +192,7 @@ test('<SelectField checkboxes> - renders a set of checkboxes with correct value 
     expect(wrapper.find(RadioButtonMaterial).at(1).prop('checked')).toBe(false);
 });
 
-test('<SelectField checkboxes> - renders a set of checkboxes with correct value (model)', () => {
+test('<SelectField checkboxes> - renders a set of RadioButtons with correct value (model)', () => {
     const element = <SelectField checkboxes name="x" />;
     const wrapper = mount(element, createContext({x: {type: String, allowedValues: ['a', 'b']}}, {model: {x: 'b'}}));
 
@@ -238,7 +201,7 @@ test('<SelectField checkboxes> - renders a set of checkboxes with correct value 
     expect(wrapper.find(RadioButtonMaterial).at(1).prop('checked')).toBe(true);
 });
 
-test('<SelectField checkboxes> - renders a set of checkboxes with correct value (specified)', () => {
+test('<SelectField checkboxes> - renders a set of RadioButtons with correct value (specified)', () => {
     const element = <SelectField checkboxes name="x" value="b" />;
     const wrapper = mount(element, createContext({x: {type: String, allowedValues: ['a', 'b']}}));
 
@@ -247,47 +210,47 @@ test('<SelectField checkboxes> - renders a set of checkboxes with correct value 
     expect(wrapper.find(RadioButtonMaterial).at(1).prop('checked')).toBe(true);
 });
 
-test('<SelectField checkboxes> - renders a set of checkboxes which correctly reacts on change', () => {
+test('<SelectField checkboxes> - renders a set of RadioButtons which correctly reacts on change', () => {
     const onChange = jest.fn();
 
     const element = <SelectField checkboxes name="x" />;
     const wrapper = mount(element, createContext({x: {type: String, allowedValues: ['a', 'b']}}, {onChange}));
 
     expect(wrapper.find(RadioButtonMaterial)).toHaveLength(2);
-    touchTap(wrapper.find(RadioButtonMaterial).at(1));
+    wrapper.find(RadioButtonMaterial).at(1).find('input').simulate('change');
     expect(onChange).toHaveBeenLastCalledWith('x', 'b');
 });
 
-test('<SelectField checkboxes> - renders a set of checkboxes which correctly reacts on change (array check)', () => {
+test('<SelectField checkboxes> - renders a set of RadioButtons which correctly reacts on change (array check)', () => {
     const onChange = jest.fn();
 
     const element = <SelectField checkboxes name="x" />;
     const wrapper = mount(element, createContext({x: {type: Array}, 'x.$': {type: String, allowedValues: ['a', 'b']}}, {onChange})); // eslint-disable-line max-len
 
     expect(wrapper.find(CheckboxMaterial)).toHaveLength(2);
-    touchTap(wrapper.find(CheckboxMaterial).at(1));
+    wrapper.find(CheckboxMaterial).at(1).find('input').simulate('change');
     expect(onChange).toHaveBeenLastCalledWith('x', ['b']);
 });
 
-test('<SelectField checkboxes> - renders a set of checkboxes which correctly reacts on change (array uncheck)', () => {
+test('<SelectField checkboxes> - renders a set of RadioButtons which correctly reacts on change (array uncheck)', () => { // eslint-disable-line max-len
     const onChange = jest.fn();
 
     const element = <SelectField checkboxes name="x" value={['b']} />;
     const wrapper = mount(element, createContext({x: {type: Array}, 'x.$': {type: String, allowedValues: ['a', 'b']}}, {onChange})); // eslint-disable-line max-len
 
     expect(wrapper.find(CheckboxMaterial)).toHaveLength(2);
-    touchTap(wrapper.find(CheckboxMaterial).at(1));
+    wrapper.find(CheckboxMaterial).at(1).find('input').simulate('change');
     expect(onChange).toHaveBeenLastCalledWith('x', []);
 });
 
-test('<SelectField checkboxes> - renders a set of checkboxes which correctly reacts on change (same value)', () => {
+test('<SelectField checkboxes> - renders a set of RadioButtons which correctly reacts on change (same value)', () => {
     const onChange = jest.fn();
 
     const element = <SelectField checkboxes name="x" />;
     const wrapper = mount(element, createContext({x: {type: String, allowedValues: ['a', 'b']}}, {model: {x: 'b'}, onChange})); // eslint-disable-line max-len
 
     expect(wrapper.find(RadioButtonMaterial)).toHaveLength(2);
-    touchTap(wrapper.find(RadioButtonMaterial).at(0));
+    wrapper.find(RadioButtonMaterial).at(0).find('input').simulate('change');
     expect(onChange).toHaveBeenLastCalledWith('x', 'a');
 });
 
