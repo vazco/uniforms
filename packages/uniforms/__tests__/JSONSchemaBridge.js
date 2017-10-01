a20057920faf04985af2615455652ca83559cc8e
Jan Owsiany <jan.owsiany@gmail.com>
2017-10-01 19:19:45 +0200

    Initial work for JSONSchemaBridge

---
+++ packages/uniforms/__tests__/JSONSchemaBridge.js
@@ -0,0 +1,59 @@
+import JSONSchemaBridge from 'uniforms/JSONSchemaBridge';
+
+describe('JSONSchemaBridge', () => {
+    const schema = {
+        $schema: 'http://json-schema.org/draft-06/schema#',
+        definitions: {
+            address: {
+                type: 'object',
+                properties: {
+                    city:   {type: 'string'},
+                    state:  {type: 'string'},
+                    street: {type: 'string'}
+                },
+                required: ['street', 'city', 'state']
+            }
+        },
+        type: 'object',
+        properties: {
+            billingAddress: {$ref: '#/definitions/address'},
+            shippingAddress: {
+                allOf: [
+                    {$ref: '#/definitions/address'},
+                    {
+                        properties: {
+                            type: {enum: ['residential', 'business']}
+                        },
+                        required: ['type']
+                    }
+                ]
+            }
+        }
+    };
+
+    const bridge = new JSONSchemaBridge(schema);
+
+    describe('#check()', () => {
+        it('works correctly with schema', () => {
+            expect(JSONSchemaBridge.check(schema)).toBeTruthy();
+        });
+
+        it('works correctly without schema', () => {
+            expect(JSONSchemaBridge.check()).not.toBeTruthy();
+        });
+    });
+
+    describe('#getField', () => {
+        it('returns correct definition (flat)', () => {
+            expect(bridge.getField('billingAddress')).toEqual({
+                properties: expect.objectContaining({
+                    city: expect.any(Object),
+                    state: expect.any(Object),
+                    street: expect.any(Object)
+                }),
+                required: expect.arrayContaining(['street', 'city', 'state']),
+                type: 'object'
+            });
+        });
+    });
+});
--- packages/uniforms/src/JSONSchemaBridge.js
+++ packages/uniforms/src/JSONSchemaBridge.js
@@ -1,4 +1,5 @@
 import invariant from 'fbjs/lib/invariant';
+import joinName  from './joinName';
 
 import Bridge from './Bridge';
 
@@ -8,35 +9,89 @@ try {
     Ajv = r('ajv');
 } catch (_) { /* Ignore it. */ }
 
-const isSS = schema => ['pick', 'omit', '_constructorOptions', '_validators', '_docValidators', '_validationContexts', '_cleanOptions', '_schema', '_schemaKeys', '_autoValues', '_blackboxKeys', '_firstLevelSchemaKeys', '_depsLabels', '_objectKeys', 'messageBox', 'version'].every(property => schema[property]);
-const isSS2 = schema => ['_schema', '_schemaKeys', '_autoValues', '_blackboxKeys', '_validators', '_messages', '_depsMessages', '_depsLabels', '_firstLevelSchemaKeys', '_objectKeys', '_validationContexts'].every(property => schema[property]);
+const SSProperties = ['_cleanOptions','_constructorOptions','_docValidators','messageBox','omit','pick','version'];
+const SS2Properties = ['_depsMessages','_messages'];
+const SSCommonProperties = [
+    '_autoValues',
+    '_blackboxKeys',
+    '_depsLabels',
+    '_firstLevelSchemaKeys',
+    '_objectKeys',
+    '_schema',
+    '_schemaKeys',
+    '_validationContexts',
+    '_validators'
+];
+
+const isSS = schema => [...SSCommonProperties, ...SSProperties].every(property => schema[property]);
+const isSS2 = schema => [...SSCommonProperties, ...SS2Properties].every(property => schema[property]);
+
+const resolveRef = (referance, schema) => {
+    invariant(
+        referance.startsWith('#'),
+        'Reference is not an internal reference, and only such are allowed: "%s"',
+        referance
+    );
+
+    const resolvedReference = referance
+        .split('/')
+        .filter(part => part && part !== '#')
+        .reduce((definition, next) => definition[next], schema);
+
+    invariant(resolvedReference, 'Reference not found in schema: "%s"', referance);
+
+    return resolvedReference;
+};
 
 export default class JSONSchemaBridge extends Bridge {
     constructor (schema) {
         super();
+
+        invariant(schema.properties, 'Schema does not contain properties field');
 
         this.schema = schema;
         this.validator = new Ajv({allErrors: true}).compile(schema);
     }
 
     static check (schema) {
-        return Ajv && new Ajv().validateSchema(schema) && (schema.$schema || !isSS(schema) && !isSS2(schema));
+        return Ajv && schema && new Ajv().validateSchema(schema) && !(isSS(schema) || isSS2(schema));
     }
 
     getError (/* name, error */) {
-        return 1;
+        return;
     }
 
     getErrorMessage (/* name, error */) {
-        return 1;
+        return;
     }
 
     getErrorMessages (/* error */) {
-        return 1;
+        return;
     }
 
     getField (name) {
-        return 1;
+        return joinName(null, name).reduce(
+            (definition, next) => {
+                if (next === '$' || next === '' + parseInt(next, 10)) {
+                    invariant(definition.type === 'array', 'Field not found in schema: "%s"', name);
+                }
+
+                definition = definition[next];
+
+                invariant(definition, 'Field not found in schema: "%s"', name);
+
+                if (definition.$ref) {
+                    definition = resolveRef(definition.$ref, this.schema);
+                }
+
+                ['allOf', 'anyOf', 'oneOf'].filter(keyword => definition[keyword]).forEach(key => {
+                    definition[key] = definition[key].map(def => def.$ref ? resolveRef(def.$ref, this.schema) : def);
+                });
+
+                return definition;
+            },
+            this.schema.properties
+        );
     }
 
     getInitialValue (name) {
@@ -47,12 +102,22 @@ export default class JSONSchemaBridge extends Bridge {
         return 1;
     }
 
-    getSubfields (/* name */) {
-        return 1;
+    getSubfields (name) {
+        if (!name) {
+            return Object.keys(this.schema.properties);
+        }
+
+        const {type: fieldType, properties: fieldProperties} = this.getField(name);
+
+        if (fieldType === 'object') {
+            return Object.keys(fieldProperties);
+        }
+
+        return [];
     }
 
     getType (name) {
-        const fieldType = this.getField(name).type;
+        const {type: fieldType} = this.getField(name);
 
         if (fieldType === 'string')                    return String;
         if (fieldType === 'object')                    return Object;
