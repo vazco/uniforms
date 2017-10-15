b2f9c02da06ca3bb823cb4d6e1d1725f20c1a21a
Jan Owsiany <jan.owsiany@gmail.com>
2017-10-15 17:10:13 +0200

    SelectField

--- packages/uniforms-material/src/SelectField.js
+++ packages/uniforms-material/src/SelectField.js
@@ -1,12 +1,12 @@
-import Checkbox       from 'material-ui/Checkbox';
-import connectField   from 'uniforms/connectField';
-import filterDOMProps from 'uniforms/filterDOMProps';
-import MenuItem       from 'material-ui/MenuItem';
-import RadioButton    from 'material-ui/RadioButton';
-import React          from 'react';
-import SelectField    from 'material-ui/SelectField';
-import Subheader      from 'material-ui/Subheader';
-import {List}         from 'material-ui/List';
+import Checkbox                                                              from 'material-ui/Checkbox';
+import connectField                                                          from 'uniforms/connectField';
+import filterDOMProps                                                        from 'uniforms/filterDOMProps';
+import Input, {InputLabel}                                                   from 'material-ui/Input';
+import PropTypes                                                             from 'prop-types';
+import React                                                                 from 'react';
+import Select                                                                from 'material-ui/Select';
+import {FormControl, FormControlLabel, FormGroup, FormHelperText, FormLabel} from 'material-ui/Form';
+import {MenuItem}                                                            from 'material-ui/Menu';
 
 const xor = (item, array) => {
     const index = array.indexOf(item);
@@ -20,93 +20,85 @@ const xor = (item, array) => {
 const renderCheckboxes = ({
     allowedValues,
     disabled,
-    fieldType,
-    id,
-    label,
-    labelPosition,
-    name,
-    onChange,
-    transform,
-    value,
-    ...props
-}) =>
-    <List {...filterDOMProps(props)}>
-        {!!label && <Subheader children={label} style={{paddingLeft: 0}} />}
-
-        {allowedValues.map(item => fieldType === Array ? (
-            <Checkbox
-                checked={value.includes(item)}
-                disabled={disabled}
-                id={`${id}-${item}`}
-                key={item}
-                label={transform ? transform(item) : item}
-                labelPosition={labelPosition}
-                name={name}
-                onCheck={() => onChange(xor(item, value))}
-            />
-        ) : (
-            <RadioButton
-                checked={value === item}
-                disabled={disabled}
-                id={`${id}-${item}`}
-                key={item}
-                label={transform ? transform(item) : item}
-                labelPosition={labelPosition}
-                name={name}
-                onCheck={() => onChange(item)}
-            />
-        ))}
-    </List>
-;
-
-const renderSelect = ({
-    allowedValues,
-    disabled,
     error,
     errorMessage,
-    fieldType,
-    fullWidth = true,
+    filter,
+    hideFiltered,
     id,
-    inputRef,
     label,
     name,
     onChange,
-    placeholder,
+    required,
     showInlineError,
     transform,
     value,
     ...props
-}) =>
-    <SelectField
-        disabled={disabled}
-        errorText={error && showInlineError ? errorMessage : undefined}
-        floatingLabelText={label}
-        fullWidth={fullWidth}
-        hintText={placeholder}
-        id={id}
-        multiple={fieldType === Array}
-        name={name}
-        onChange={(event, index, value) => onChange(value)}
-        ref={inputRef}
-        value={value}
-        {...filterDOMProps(props)}
-    >
-        {allowedValues.map(allowedValue =>
-            <MenuItem
-                insetChildren={fieldType === Array || undefined}
-                checked={fieldType === Array && value && value.includes(allowedValue) || undefined}
-                key={allowedValue}
-                primaryText={transform ? transform(allowedValue) : allowedValue}
-                value={allowedValue}
-            />
-        )}
-    </SelectField>
-;
+}) => (
+    <FormControl component="fieldset" disabled={disabled} error={!!error} required={required}>
+        {label && <FormLabel component="legend">{label}</FormLabel>}
+        <FormGroup id={id} name={name}>
+            {(hideFiltered && filter ? allowedValues.filter(filter) : allowedValues).map(item =>
+                <FormControlLabel
+                    control={<Checkbox
+                        checked={value.includes(item)}
+                        onChange={() => onChange(xor(item, value))}
+                        value={item}
+                        {...filterDOMProps(props)}
+                    />}
+                    disabled={disabled || !filter(item)}
+                    key={item}
+                    label={transform ? transform(item) : item}
+                    value={item}
+                />
+            )}
+        </FormGroup>
+        {error && showInlineError && <FormHelperText>{errorMessage}</FormHelperText>}
+    </FormControl>
+);
 
-const Select = ({checkboxes, ...props}) =>
-    checkboxes
-        ? renderCheckboxes(props)
-        : renderSelect    (props)
-;
+const renderSelect = ({
+    allowedValues,
+    disabled,
+    displayEmpty,
+    error,
+    errorMessage,
+    filter,
+    id,
+    label,
+    MenuProps,
+    multiple,
+    onChange,
+    renderValue,
+    required,
+    showInlineError,
+    transform,
+    value,
+    ...props
+}) => (
+    <FormControl disabled={disabled} error={!!error} required={required}>
+        {label && <InputLabel htmlFor={id}>{label}</InputLabel>}
+        <Select
+            displayEmpty={displayEmpty}
+            input={<Input id={id} {...filterDOMProps(props)} />}
+            MenuProps={MenuProps}
+            multiple={multiple}
+            onChange={event => onChange(event.target.value)}
+            renderValue={renderValue}
+            value={value}
+        >
+            {(filter ? filter.filter(filter) : allowedValues).map(item =>
+                <MenuItem key={item} value={item}>{transform ? transform(item) : item}</MenuItem>
+            )}
+        </Select>
+        {error && showInlineError && <FormHelperText>{errorMessage}</FormHelperText>}
+    </FormControl>
+);
 
-export default connectField(Select, {ensureValue: false});
+const Select_ = ({checkboxes, ...props}) => checkboxes ? renderCheckboxes(props) : renderSelect(props);
+
+Select_.propTypes = {
+    filter: PropTypes.func,
+    hideFiltered:   PropTypes.bool
+};
+
+export default connectField(Select_);
