35e8d35f335f1eaa1b12054e1031f2d561480527
Jan Owsiany <jan.owsiany@gmail.com>
2018-02-06 09:40:45 +0100

    Added JSON schema to the demo app.

--- demo/imports/lib/presets.js
+++ demo/imports/lib/presets.js
@@ -92,6 +92,32 @@ const presets = {
         })
     `,
 
+    'Address (JSONSchema)': preset`
+        schema = {
+            title: 'Address',
+            type: 'object',
+            properties: {
+                city:   {type: 'string'},
+                state:  {type: 'string'},
+                street: {type: 'string'},
+                zip:    {type: 'string', pattern: '[0-9]{5}'},
+            },
+            required: ['street', 'zip', 'state']
+        },
+
+        validator = new Ajv({allErrors: true, useDefaults: true}).compile(schema),
+
+        schemaValidator = model => {
+            validator(model);
+
+            if (validator.errors && validator.errors.length) {
+                throw {details: validator.errors};
+            }
+        },
+
+        new JSONSchemaBridge(schema, schemaValidator)
+    `,
+
     'Address (SimpleSchema)': preset`
         new SimpleSchema({
             city: {
