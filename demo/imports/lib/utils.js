40d31f015e50e929421c670fb6cc5dd46904184b
Jan Owsiany <jan.owsiany@gmail.com>
2018-07-10 11:14:02 +0200

    Added demo app state persistence in the url.

---
+++ demo/imports/lib/utils.js
@@ -0,0 +1,57 @@
+const URL_KEYS = ['preset', 'props',  'theme'];
+
+export const updateQuery = state => {
+    const query = URL_KEYS.map(key => {
+        if (!state[key]) {
+            return null;
+        }
+
+        if (key === 'props') {
+            return key + '=' + JSON.stringify(state[key]);
+        }
+
+        return key + '=' + window.encodeURIComponent(state[key]);
+    })
+        .filter(Boolean)
+        .join('&');
+
+    window.location.hash = '?' + query;
+};
+
+const mergeDefinedKeys = (raw, keys, target) => {
+    keys.forEach(key => {
+        if (raw[key] !== null) {
+            target[key] = raw[key];
+        }
+    });
+};
+
+export const parseQuery = () => {
+    const raw = document.location.hash
+        .replace(/^#\?/, '')
+        .split('&')
+        .reduce((reduced, pair) => {
+            const pieces = pair.split('=');
+            const name = window.decodeURIComponent('' + pieces[0]);
+
+            let value = window.decodeURIComponent('' + pieces[1]);
+
+            if (value === 'true' || value === 'false') {
+                value = value === 'true';
+            }
+
+            if (name === 'props') {
+                reduced[name] = JSON.parse(value);
+            } else {
+                reduced[name] = value;
+            }
+
+            return reduced;
+        }, {});
+
+    const state = {};
+
+    mergeDefinedKeys(raw, URL_KEYS, state);
+
+    return state;
+};
