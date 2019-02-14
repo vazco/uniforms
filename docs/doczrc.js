8c71c44bd267883bd251b343f378c60221039bf1
Radosław Miernik <radekmie@gmail.com>
2019-02-14 10:10:15 +0100

    Added automatic docs deploy.

--- docs/doczrc.js
+++ docs/doczrc.js
@@ -1,8 +1,10 @@
 import {css} from 'docz-plugin-css';
 
 export default {
+  base: '/uniforms/',
   title: 'uniforms',
   description: 'uniforms: A set of React libraries for building forms.',
+  hashRouter: true,
   codeSandbox: false,
   propsParser: false,
   plugins: [css({preprocessor: 'postcss'})],
