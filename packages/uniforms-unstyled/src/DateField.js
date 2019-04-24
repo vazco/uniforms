3c0a974d12ed089d2ec56b9dc461ce97aa6f033b
Marco Wettstein <macrozone@gmail.com>
2019-04-24 11:04:21 +0200

    fix: Datefield prints date in wrong timezone

--- packages/uniforms-unstyled/src/DateField.js
+++ packages/uniforms-unstyled/src/DateField.js
@@ -2,10 +2,17 @@ import React from 'react';
 import connectField from 'uniforms/connectField';
 import filterDOMProps from 'uniforms/filterDOMProps';
 
+function convertDateToLocalString(dateInUtc) {
+  //Convert to local timezone
+  return new DateConstructor(dateInUtc.getTime() - dateInUtc.getTimezoneOffset() * 60 * 1000)
+    .toISOString()
+    .slice(0, -8);
+}
+
 const DateConstructor = (typeof global === 'object' ? global : window).Date;
-const dateFormat = value => value && value.toISOString().slice(0, -8);
+const dateFormat = value => value && convertDateToLocalString(value);
 const dateParse = (timestamp, onChange) => {
-  const date = new DateConstructor(timestamp);
+  const date = new DateConstructor(timestamp + new DateConstructor().getTimezoneOffset() * 60 * 1000);
   if (date.getFullYear() < 10000) {
     onChange(date);
   } else if (isNaN(timestamp)) {
