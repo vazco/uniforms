54415940f26bdfaa3d85cb4776535ad53dcbf684
Adrian Mucha <kestarumper8@gmail.com>
2019-07-12 17:02:19 +0200

    add tabs to installation section

---
+++ docs/components/Code.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import Highlight, { defaultProps } from 'prism-react-renderer';
+import nightOwl from 'prism-react-renderer/themes/nightOwl';
+
+function Code({ children, language }) {
+  return (
+    <Highlight
+      {...defaultProps}
+      theme={nightOwl}
+      code={children}
+      language={language}
+    >
+      {({ className, style, tokens, getLineProps, getTokenProps }) => (
+        <pre className={className} style={{ ...style, padding: '20px' }}>
+          {tokens.map((line, i) => (
+            <div key={i} {...getLineProps({ line, key: i })}>
+              {line.map((token, key) => (
+                <span key={key} {...getTokenProps({ token, key })} />
+              ))}
+            </div>
+          ))}
+        </pre>
+      )}
+    </Highlight>
+  );
+}
+
+export default Code;
