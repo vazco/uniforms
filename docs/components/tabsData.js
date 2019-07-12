cadc371e84df1010800d80c75dc70b7b37d3a70c
Adrian Mucha <adrian.mucha@vazco.eu>
2019-07-12 15:06:52 +0200

    refactor tabs

--- docs/components/tabsData.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-export default [
-  { name: 'GraphQL', content: <h1>Tabs1</h1> },
-  { name: 'JSON', content: <h1>Tabs2</h1> },
-  { name: 'SimpleSchema', content: <h1>Tabs3</h1> }
-];
