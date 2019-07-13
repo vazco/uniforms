139ef79ab370748f36f7ce8f56585256492445c1
Kris <szut.krzysztof@gmail.com>
2019-07-13 17:11:20 +0200

    Added section about customizing displayed errors.

--- docs/components/GuestForm5.js
+++ docs/components/GuestForm5.js
@@ -1,9 +1,8 @@
+import AutoField from 'uniforms-unstyled/AutoField';
 import AutoForm from 'uniforms-unstyled/AutoForm';
 import ErrorField from 'uniforms-unstyled/ErrorField';
-import NumField from 'uniforms-unstyled/NumField';
 import React from 'react';
 import SubmitField from 'uniforms-unstyled/SubmitField';
-import TextField from 'uniforms-unstyled/TextField';
 
 import GuestSchema from './GuestSchema';
 
@@ -11,13 +10,21 @@ export default function GuestForm5({ onSubmit }) {
   return (
     <AutoForm schema={GuestSchema} onSubmit={onSubmit}>
       <h4>IT meeting guest questionnaire</h4>
-      <TextField name="lastName" />
-      <ErrorField name="lastName" />
-      <TextField name="firstName" />
-      <ErrorField name="firstName" />
+      <AutoField name="lastName" />
+      <ErrorField name="lastName">
+        <span>You have to provide your last name!</span>
+      </ErrorField>
+      <AutoField name="firstName" />
+      <ErrorField
+        name="firstName"
+        errorMessage="You have to provide your first name!"
+      />
       <span>Do you want to share your work experience with us?</span>
-      <NumField name="workExperience" />
-      <ErrorField name="workExperience" />
+      <AutoField name="workExperience" />
+      <ErrorField
+        name="workExperience"
+        errorMessage="Your work experience cannot be lesser than 0 or greater than 100 years!"
+      />
       <SubmitField />
     </AutoForm>
   );
