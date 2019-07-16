8af90f8da0edf024d5667360d36c0656a357b76d
Krzysztof Szut <szut.krzysztof@gmail.com>
2019-07-16 09:46:08 +0200

    Modified ImageField.

--- docs/components/ImageField.js
+++ docs/components/ImageField.js
@@ -1,8 +1,9 @@
 import React from 'react';
-
 import connectField from 'uniforms/connectField';
 
 function Image({ onChange, value }) {
+  const imgPlaceholder = 'https://via.placeholder.com/150.png';
+
   function onImageChange({ target: { files } }) {
     if (files && files[0]) {
       onChange(URL.createObjectURL(files[0]));
@@ -10,10 +11,21 @@ function Image({ onChange, value }) {
   }
 
   return (
-    <div>
-      <label>Choose your photo</label>
-      <input accept="image/*" onChange={onImageChange} type="file" />
-      <img src={value} />
+    <div class="ImageField">
+      <label htmlFor="file-input">
+        <div>Choose your photo</div>
+        <img
+          style={{ cursor: 'pointer', width: '150px', height: '150px' }}
+          src={value ? value : imgPlaceholder}
+        />
+      </label>
+      <input
+        accept="image/*"
+        id="file-input"
+        onChange={onImageChange}
+        style={{ display: 'none' }}
+        type="file"
+      />
     </div>
   );
 }
