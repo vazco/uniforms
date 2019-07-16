224864fa5eb949c98f43a14d7413f229838b4c21
Adrian Mucha <kestarumper8@gmail.com>
2019-07-16 09:59:42 +0200

    Changed beheviour of toggler component.

--- docs/components/Toggler/TogglerHeader.js
+++ docs/components/Toggler/TogglerHeader.js
@@ -1,45 +1,53 @@
 import React, { useState } from 'react';
 import './Toggler.css';
 import TogglerHeaderItem from './TogglerHeaderItem';
+import PropTypes from 'prop-types';
 
 function getDefaultToggle(items) {
-  return items
-    .filter(({ active }) => active)
-    .reduce((acc, { key }) => Object.assign(acc, { [key]: true }), {});
+  return items.find(({ active }) => active === true) || items[0] || {};
 }
 
 function TogglerHeader({ items }) {
-  const [state, setState] = useState(getDefaultToggle(items));
+  const [state, setState] = useState({
+    activeKey: getDefaultToggle(items).key,
+    activeComponent: getDefaultToggle(items).component
+  });
 
-  const toggleClick = key => () => {
-    setState(prevState => ({
-      ...prevState,
-      [key]: !prevState[key]
-    }));
+  const toggleClick = (key, component) => () => {
+    setState({
+      activeKey: key,
+      activeComponent: component
+    });
   };
 
+  const { activeKey, activeComponent } = state;
   return (
     <>
       <section className="Toggler header">
-        {items.map(({ key, tooltipText, icon }) => (
+        {items.map(({ key, tooltipText, icon, component }) => (
           <TogglerHeaderItem
             key={key}
             icon={icon}
             tooltipText={tooltipText}
-            active={state[key]}
-            onClick={toggleClick(key)}
+            active={activeKey === key}
+            onClick={toggleClick(key, component)}
           />
         ))}
       </section>
-      <section>
-        {items
-          .filter(({ key }) => state[key] === true)
-          .map(({ key, component }) => (
-            <span key={key}>{component}</span>
-          ))}
-      </section>
+      <section>{activeComponent}</section>
     </>
   );
 }
+
+TogglerHeader.propTypes = {
+  items: PropTypes.arrayOf(
+    PropTypes.shape({
+      key: PropTypes.string.isRequired,
+      tooltipText: PropTypes.string.isRequired,
+      icon: PropTypes.element.isRequired,
+      component: PropTypes.element.isRequired
+    })
+  ).isRequired
+};
 
 export default TogglerHeader;
