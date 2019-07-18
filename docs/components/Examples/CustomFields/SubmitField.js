1f03aa91924857494352aa09cf8606669416cc98
Adrian Mucha <kestarumper8@gmail.com>
2019-07-18 16:38:12 +0200

    Added rendered output for Submit field example.

---
+++ docs/components/Examples/CustomFields/SubmitField.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import BaseField from 'uniforms/BaseField';
+import connectField from 'uniforms/connectField';
+
+import schema from './CompositeFieldSchema';
+import { AutoField, AutoForm } from '../../universal';
+
+// This field works as follows: render standard submit field and disable it, when
+// the form is invalid. It's a simplified version of a default SubmitField from
+// uniforms-unstyled.
+const SubmitField = (
+  props,
+  {
+    uniforms: {
+      error,
+      state: { disabled, submitting, validating }
+    }
+  }
+) => (
+  <input
+    disabled={!!(error || disabled || submitting || validating)}
+    type="submit"
+  />
+);
+SubmitField.contextTypes = BaseField.contextTypes;
+
+// This field is a kind of a shortcut for few fields. You can also access all
+// field props here, like value or onChange for some extra logic.
+const Composite = () => (
+  <section>
+    <AutoField name="firstName" />
+    <AutoField name="lastName" />
+    <AutoField name="workExperience" />
+    <hr />
+  </section>
+);
+
+const CompositeField = connectField(Composite);
+
+export default function ExamplesSubmitField() {
+  return (
+    <AutoForm schema={schema}>
+      <CompositeField name="personA" />
+      <CompositeField name="personB" />
+      <SubmitField />
+    </AutoForm>
+  );
+}
