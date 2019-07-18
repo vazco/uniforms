19a1c5a73c6dbc14e534b1296a45eec8f2c70fac
Adrian Mucha <kestarumper8@gmail.com>
2019-07-18 16:09:53 +0200

    Added rendered output for Rating field example.

---
+++ docs/components/Examples/CustomFields/RatingField.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import classnames from 'classnames';
+import connectField from 'uniforms/connectField';
+
+import { AutoForm, SubmitField } from '../../universal';
+import schema from './RatingFieldSchema';
+
+const Rating = ({
+  className,
+  disabled,
+  max = 5,
+  required,
+  value,
+  onChange
+}) => (
+  <section className={classnames('ui', { disabled, required }, className)}>
+    {[...Array(max)]
+      .map((_, index) => index + 1)
+      .map(index => (
+        <span
+          style={{ fontSize: 40 }}
+          key={index}
+          onClick={() =>
+            disabled || onChange(!required && value === index ? null : index)
+          }
+        >
+          {index <= value ? '★' : '☆'}
+        </span>
+      ))}
+  </section>
+);
+
+const RatingField = connectField(Rating);
+
+export default function ExamplesRangeField() {
+  return (
+    <AutoForm schema={schema}>
+      <RatingField name="rating" />
+      <SubmitField />
+    </AutoForm>
+  );
+}
---
+++ docs/components/Examples/CustomFields/RatingFieldSchema.js
@@ -0,0 +1,26 @@
+import Ajv from 'ajv';
+
+import { JSONSchemaBridge } from 'uniforms-bridge-json-schema';
+
+const schema = {
+  title: 'Rating',
+  type: 'object',
+  properties: {
+    rating: {
+      type: 'integer'
+    }
+  }
+};
+
+const validator = new Ajv({ allErrors: true, useDefaults: true }).compile(
+  schema
+);
+
+const schemaValidator = model => {
+  validator(model);
+  if (validator.errors && validator.errors.length) {
+    throw { details: validator.errors };
+  }
+};
+
+export default new JSONSchemaBridge(schema, schemaValidator);
