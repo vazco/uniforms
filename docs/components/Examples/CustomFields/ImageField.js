8c5e669bb5cfa8ed403095838a7749eade102cd8
Adrian Mucha <kestarumper8@gmail.com>
2019-07-18 17:02:12 +0200

    Added rendered output for Image field example.

---
+++ docs/components/Examples/CustomFields/ImageField.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import connectField from 'uniforms/connectField';
+
+import schema from './ImageFieldSchema';
+import { AutoForm, SubmitField } from '../../universal';
+
+function Image({ onChange, value }) {
+  const imgPlaceholder = 'https://via.placeholder.com/150.png';
+
+  function onImageChange({ target: { files } }) {
+    if (files && files[0]) {
+      onChange(URL.createObjectURL(files[0]));
+    }
+  }
+
+  return (
+    <div className="ImageField">
+      <label htmlFor="file-input">
+        <div>Choose your photo</div>
+        <img
+          style={{ cursor: 'pointer', width: '150px', height: '150px' }}
+          src={value ? value : imgPlaceholder}
+        />
+      </label>
+      <input
+        accept="image/*"
+        id="file-input"
+        onChange={onImageChange}
+        style={{ display: 'none' }}
+        type="file"
+      />
+    </div>
+  );
+}
+const ImageField = connectField(Image);
+
+export default function ExamplesSubmitField() {
+  return (
+    <AutoForm schema={schema}>
+      <ImageField name="pictureUrl" />
+      <SubmitField />
+    </AutoForm>
+  );
+}
---
+++ docs/components/Examples/CustomFields/ImageFieldSchema.js
@@ -0,0 +1,28 @@
+import Ajv from 'ajv';
+
+import { JSONSchemaBridge } from 'uniforms-bridge-json-schema';
+
+const schema = {
+  title: 'Guest',
+  type: 'object',
+  properties: {
+    pictureUrl: {
+      type: 'string'
+    }
+  }
+};
+
+const validator = new Ajv({ allErrors: true, useDefaults: true }).compile(
+  schema
+);
+
+const schemaValidator = model => {
+  validator(model);
+  if (validator.errors && validator.errors.length) {
+    throw { details: validator.errors };
+  }
+};
+
+const bridge = new JSONSchemaBridge(schema, schemaValidator);
+
+export default bridge;
