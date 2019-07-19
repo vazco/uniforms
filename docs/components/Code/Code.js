a4e8f9c733b809a758f922dd536d9374c707f321
Adrian Mucha <kestarumper8@gmail.com>
2019-07-19 12:50:09 +0200

    Changed Code component to use built in CodeBlock from Docusaurus.

--- docs/components/Code/Code.js
+++ docs/components/Code/Code.js
@@ -1,28 +1,8 @@
 import React from 'react';
-import Highlight, { defaultProps } from 'prism-react-renderer';
-import nightOwl from 'prism-react-renderer/themes/nightOwl';
+import CodeBlock from '@theme/CodeBlock';
 
 function Code({ children, language }) {
-  return (
-    <Highlight
-      {...defaultProps}
-      theme={nightOwl}
-      code={children}
-      language={language}
-    >
-      {({ className, style, tokens, getLineProps, getTokenProps }) => (
-        <pre className={className} style={{ ...style, padding: '20px' }}>
-          {tokens.map((line, i) => (
-            <div key={i} {...getLineProps({ line, key: i })}>
-              {line.map((token, key) => (
-                <span key={key} {...getTokenProps({ token, key })} />
-              ))}
-            </div>
-          ))}
-        </pre>
-      )}
-    </Highlight>
-  );
+  return <CodeBlock children={children} className={language} />;
 }
 
 export default Code;
