54bcbe37e593b12174e5b428007169c0f96b7fba
Adrian Mucha <kestarumper8@gmail.com>
2019-07-19 16:43:20 +0200

    Added pointer cursor to SwapField.

--- docs/components/Examples/CustomFields/SwapField.js
+++ docs/components/Examples/CustomFields/SwapField.js
@@ -10,27 +10,31 @@ import schema from './SwapFieldSchema';
 const SwapField = (
   { children, fieldA, fieldB },
   { uniforms: { model, onChange } }
-) =>
-  cloneElement(Children.only(children), {
-    onClick() {
-      const valueA = get(model, fieldA);
-      const valueB = get(model, fieldB);
-      onChange(fieldA, valueB);
-      onChange(fieldB, valueA);
-    }
-  });
+) => (
+  <span style={{ display: 'flex', justifyContent: 'center' }}>
+    {cloneElement(Children.only(children), {
+      onClick() {
+        const valueA = get(model, fieldA);
+        const valueB = get(model, fieldB);
+        onChange(fieldA, valueB);
+        onChange(fieldB, valueA);
+      }
+    })}
+  </span>
+);
 SwapField.contextTypes = BaseField.contextTypes;
 // Usage.
 export default function ExampleSwapField() {
   return (
     <section>
       <AutoForm
+        model={{ firstName: 'John', lastName: 'Doe' }}
         schema={schema}
         onSubmit={model => alert(JSON.stringify(model, null, 2))}
       >
         <TextField name="firstName" />
         <SwapField fieldA="firstName" fieldB="lastName">
-          <RefreshCw />
+          <RefreshCw style={{ cursor: 'pointer' }} />
         </SwapField>
         <TextField name="lastName" />
         <SubmitField />
