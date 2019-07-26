bf152d608886044f047f79cea761a00539cc228e
Adrian Mucha <kestarumper8@gmail.com>
2019-07-26 16:40:57 +0200

    Added cached fetch statistics.

--- website/components/Home/Stats/Stats.js
+++ website/components/Home/Stats/Stats.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { Star, GitBranch, Download } from 'react-feather';
+import pick from 'lodash/pick';
 
 import { Row } from '../Grid';
 import styles from './Stats.module.css';
@@ -13,14 +14,34 @@ function StatisticItem({ children, value }) {
   );
 }
 
+function cachedFetch(key, url, properties) {
+  const dataKey = `__cached-fetch-${key}`;
+  const timeKey = `__time__cached-fetch-${key}`;
+  const lastUpdated = localStorage.getItem(timeKey);
+  const value = localStorage.getItem(dataKey);
+  if (value && lastUpdated && lastUpdated > Date.now()) {
+    return Promise.resolve(JSON.parse(value));
+  }
+  return fetch(url)
+    .then(res => res.json())
+    .then(data => (properties ? pick(data, properties) : data))
+    .then(data => {
+      localStorage.setItem(dataKey, JSON.stringify(data));
+      localStorage.setItem(timeKey, Date.now() + 2 * 60 * 1000);
+      return data;
+    });
+}
+
 function useStats() {
   const [stars, setStars] = useState(null);
   const [forks, setForks] = useState(null);
   const [downloads, setDownloads] = useState(null);
 
   useEffect(() => {
-    fetch('https://api.github.com/repos/vazco/uniforms')
-      .then(res => res.json())
+    cachedFetch('github', 'https://api.github.com/repos/vazco/uniforms', [
+      'stargazers_count',
+      'forks_count'
+    ])
       // eslint-disable-next-line camelcase
       .then(({ stargazers_count, forks_count }) => {
         setStars(stargazers_count.toLocaleString('en-US'));
@@ -30,9 +51,11 @@ function useStats() {
 
   useEffect(() => {
     const today = new Date().toISOString().replace(/T.*/, '');
-    fetch(`https://api.npmjs.org/downloads/point/2016-01-01:${today}/uniforms`)
-      .then(res => res.json())
-      .then(({ downloads }) => setDownloads(downloads.toLocaleString('en-US')));
+    cachedFetch(
+      'npm',
+      `https://api.npmjs.org/downloads/point/2016-01-01:${today}/uniforms`,
+      ['downloads']
+    ).then(({ downloads }) => setDownloads(downloads.toLocaleString('en-US')));
   }, [downloads]);
 
   return {
