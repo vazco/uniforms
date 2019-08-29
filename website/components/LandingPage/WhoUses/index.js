891eda968facc21ed5d8404c3a51085d10a54204
Adrian Mucha <kestarumper8@gmail.com>
2019-08-29 09:16:57 +0200

    Added companies logos. Added add your company cta button.

--- website/components/LandingPage/WhoUses/index.js
+++ website/components/LandingPage/WhoUses/index.js
@@ -9,7 +9,18 @@ import styles from '../index.module.css';
 export default function WhyUs() {
   return (
     <Section className={styles.whouses}>
-      <div className={classNames('col col--7', styles['section-bgwhite'])} />
+      <div className={classNames('col col--7', styles['section-bgwhite'])}>
+        <div className={styles.grid3x3}>
+          {Array.from({ length: 9 }).map((_, index) => (
+            <div key={index} className={styles.item3x3}>
+              <img
+                className={styles.company}
+                src={`assets/companies/${index + 1}.png`}
+              />
+            </div>
+          ))}
+        </div>
+      </div>
       <div className="col col--3">
         <Subtitle>Lorem ipsum dolor</Subtitle>
         <Heading className={styles['heading-white']}>Who uses uniforms</Heading>
@@ -19,6 +30,14 @@ export default function WhyUs() {
           Praesent porta tempor purus, nec consectetur ligula hendrerit non.
           Pellentesque fermentum sit amet tortor in rhoncus
         </p>
+        <button
+          className={classNames(
+            'button button--lg button--primary',
+            styles['call-to-action']
+          )}
+        >
+          Add your company
+        </button>
       </div>
     </Section>
   );
