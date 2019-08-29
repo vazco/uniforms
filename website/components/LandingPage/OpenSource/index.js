b1029f93009b8a5f6d8705d07cc865d76f772ca1
Adrian Mucha <kestarumper8@gmail.com>
2019-08-29 16:11:54 +0200

    Added fetching real statistics.

--- website/components/LandingPage/OpenSource/index.js
+++ website/components/LandingPage/OpenSource/index.js
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import classNames from 'classnames';
 import { Star, GitBranch, Download } from 'react-feather';
+import pick from 'lodash/pick';
 
 import Heading from '../common/Heading';
 import Oval from '../common/Oval';
@@ -28,7 +29,77 @@ function Badge({ border, number, text, icon: Icon, color }) {
   );
 }
 
+function cacheKey(key) {
+  return `__cached-fetch-${key}`;
+}
+
+function cacheGet(key) {
+  try {
+    const { expires, data } = JSON.parse(localStorage.getItem(cacheKey(key)));
+    if (expires > Date.now()) return data;
+  } catch (error) {
+    // Nothing.
+  }
+}
+
+function cacheSet(key, data) {
+  const twoMinutes = Date.now() + 2 * 60 * 1000;
+  try {
+    localStorage.setItem(
+      cacheKey(key),
+      JSON.stringify({ data, expires: twoMinutes })
+    );
+  } catch (error) {
+    // Nothing.
+  }
+}
+
+function cachedFetch(key, url, properties) {
+  const data = cacheGet(key);
+  if (data) return Promise.resolve(data);
+
+  return fetch(url)
+    .then(response => response.json())
+    .then(data => {
+      if (properties) data = pick(data, properties);
+      cacheSet(key, data);
+      return data;
+    }, console.error);
+}
+
+function useStats() {
+  const [stars, setStars] = useState(null);
+  const [forks, setForks] = useState(null);
+  const [downloads, setDownloads] = useState(null);
+
+  useEffect(() => {
+    cachedFetch('github', 'https://api.github.com/repos/vazco/uniforms', [
+      'stargazers_count',
+      'forks_count'
+    ]).then(({ forks_count: forks, stargazers_count: stars }) => {
+      setForks(forks.toLocaleString('en-US'));
+      setStars(stars.toLocaleString('en-US'));
+    });
+  }, [stars, forks]);
+
+  useEffect(() => {
+    const today = new Date().toISOString().slice(0, 10);
+    cachedFetch(
+      'npm',
+      `https://api.npmjs.org/downloads/point/2016-01-01:${today}/uniforms`,
+      ['downloads']
+    ).then(({ downloads }) => setDownloads(downloads.toLocaleString('en-US')));
+  }, [downloads]);
+
+  return {
+    stars,
+    forks,
+    downloads
+  };
+}
+
 export default function OpenSource() {
+  const { stars, forks, downloads } = useStats();
   return (
     <div className={classNames('container', styles.section)}>
       <p className={classNames(styles.centered, styles['always-open-source'])}>
@@ -43,21 +114,21 @@ export default function OpenSource() {
         <Badge
           text="Stars"
           border={1}
-          number={740}
+          number={stars}
           icon={Star}
           color="#723CFF"
         />
         <Badge
           text="Forks"
           border={2}
-          number={131}
+          number={forks}
           icon={GitBranch}
           color="#3FBBFE"
         />
         <Badge
           text="Downloads"
           border={3}
-          number={'108,604'}
+          number={downloads}
           icon={Download}
           color="#1FD898"
         />
