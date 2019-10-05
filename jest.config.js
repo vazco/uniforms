9922d908f07566716629606e03d68974d1b90a7c
Radosław Miernik <radekmie@gmail.com>
2019-10-05 17:37:43 +0200

    Fixed globalThis in tests.

--- jest.config.js
+++ jest.config.js
@@ -8,7 +8,7 @@ module.exports = {
     '^simpl-schema$': '<rootDir>/node_modules/simpl-schema',
     '^uniforms([^/]*)(.*)$': '<rootDir>/packages/uniforms$1/src$2'
   },
-  setupFiles: ['./scripts/setupEnzyme.js', './scripts/setupPolyfill.js'],
+  setupFiles: ['./scripts/setupEnzyme.js'],
   testMatch: ['**/__tests__/**/!(_)*.{js,ts,tsx}'],
   transform: {
     '^.+\\.(js|ts|tsx)$': './scripts/transform.js'
