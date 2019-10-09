1f0c2a253d04708d0f68c6db70a07a4f758dbc42
Radosław Miernik <radekmie@gmail.com>
2019-10-09 14:29:33 +0200

    Fixed coverage regression in uniforms-antd LongTextField.

--- packages/uniforms-antd/src/LongTextField.tsx
+++ packages/uniforms-antd/src/LongTextField.tsx
@@ -6,8 +6,10 @@ import filterDOMProps from 'uniforms/filterDOMProps';
 import wrapField from './wrapField';
 
 // NOTE: Input.TextArea was introduced in 2.12.0 and removed in 3.0.0.
-const TextArea: any = Input.TextArea || Input;
-const textType = Input.TextArea ? undefined : 'textarea';
+// istanbul ignore next
+const [TextArea, textAreaProps] = Input.TextArea
+  ? [Input.TextArea, {}]
+  : [Input, { type: 'textarea' }];
 
 const LongText = props =>
   wrapField(
@@ -19,11 +21,12 @@ const LongText = props =>
       onChange={event => props.onChange(event.target.value)}
       placeholder={props.placeholder}
       ref={props.inputRef}
-      type={textType}
       value={props.value}
+      {...textAreaProps}
       {...filterDOMProps(props)}
     />
   );
+
 LongText.defaultProps = { rows: 5 };
 
 export default connectField(LongText);
