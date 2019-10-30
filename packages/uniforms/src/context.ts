9c9dbb0447b1f69ff33cd1248b35cdebb46a481e
Radosław Miernik <radekmie@gmail.com>
2019-10-30 21:20:18 +0100

    Typed context.

--- packages/uniforms/src/context.ts
+++ packages/uniforms/src/context.ts
@@ -1,5 +1,39 @@
-import { createContext } from 'react';
+// FIXME: Finish ESLint configuration for TypeScript.
+// eslint-disable-next-line no-unused-vars
+import { SyntheticEvent, createContext } from 'react';
 
-export interface Context {}
+// FIXME: Finish ESLint configuration for TypeScript.
+// eslint-disable-next-line no-unused-vars
+import Bridge from './Bridge';
 
-export default createContext<{ uniforms?: Context }>({});
+// TODO: Think about externalizing these types.
+export type DeepPartial<T> = { [P in keyof T]?: DeepPartial<T[P]> };
+export type ChangedMap<T> = T extends object
+  ? { [P in keyof T]?: ChangedMap<T[P]> }
+  : Record<string, void>;
+
+export interface UniformsContext<Model extends object = Record<string, any>> {
+  name: string[];
+  error: any;
+  model: DeepPartial<Model>;
+  schema: Bridge;
+  state: {
+    changed: boolean;
+    changedMap: ChangedMap<Model>;
+    submitting: boolean;
+    label: boolean;
+    disabled: boolean;
+    placeholder: boolean;
+    // TODO: Themes may extend the state.
+    [_: string]: unknown;
+  };
+  onChange(key: string, value: unknown): void;
+  onSubmit(event?: SyntheticEvent): Promise<unknown>;
+  randomId(): string;
+}
+
+// FIXME: This context is nullable. Should we provide a wrapper for useContext?
+// @ts-ignore
+const none: { uniforms: UniformsContext } = null;
+
+export default createContext<{ uniforms: UniformsContext }>(none);
