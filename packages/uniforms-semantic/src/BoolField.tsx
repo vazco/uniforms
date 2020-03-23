6ee4c250392f3c1b5c20c6baa117519a1a28ab5c
Konrad Bosak <konradbosak@protonmail.com>
2020-03-23 12:45:41 +0100

    Added BoolField types in uniforms-semantic.

--- packages/uniforms-semantic/src/BoolField.tsx
+++ packages/uniforms-semantic/src/BoolField.tsx
@@ -1,6 +1,15 @@
-import React from 'react';
+import React, { HTMLProps, Ref } from 'react';
 import classnames from 'classnames';
 import { connectField, filterDOMProps } from 'uniforms';
+
+type BoolFieldProps = {
+  error?: boolean;
+  errorMessage?: string;
+  showInlineError?: boolean;
+  value?: boolean;
+  onChange: (value?: boolean) => void;
+  inputRef?: Ref<HTMLInputElement>;
+} & Omit<HTMLProps<HTMLDivElement>, 'value'>;
 
 const Bool = ({
   className,
@@ -16,7 +25,7 @@ const Bool = ({
   showInlineError,
   value,
   ...props
-}) => (
+}: BoolFieldProps) => (
   <div
     className={classnames(className, { disabled, error, required }, 'field')}
     {...filterDOMProps(props)}
