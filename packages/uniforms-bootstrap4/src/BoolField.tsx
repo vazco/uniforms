16d51ca208cf6b12516f4aafacf2f9abc9ce864d
Konrad Bosak <konradbosak@protonmail.com>
2020-03-23 14:25:12 +0100

    Added BoolField types in uniforms-bootstrap4.

--- packages/uniforms-bootstrap4/src/BoolField.tsx
+++ packages/uniforms-bootstrap4/src/BoolField.tsx
@@ -1,27 +1,50 @@
-import React from 'react';
+import React, { HTMLProps, Ref } from 'react';
 import classnames from 'classnames';
 import { connectField } from 'uniforms';
 
 import wrapField from './wrapField';
 
-const Bool = ({ label, labelBefore, ...props }: any) =>
+type BoolFieldProps = {
+  labelBefore: string;
+  inputClassName?: string;
+  error?: boolean;
+  onChange: (value?: boolean) => void;
+  inputRef?: Ref<HTMLInputElement>;
+  inline?: boolean;
+  value?: boolean;
+  disabled?: boolean;
+} & Omit<HTMLProps<HTMLDivElement>, 'value'>;
+
+const Bool = ({
+  label,
+  labelBefore,
+  inputClassName,
+  disabled,
+  name,
+  error,
+  inputRef,
+  value,
+  onChange,
+  inline,
+  ...props
+}: BoolFieldProps) =>
   wrapField(
     { label: labelBefore, ...props },
     <div
-      className={classnames(props.inputClassName, 'form-check', 'checkbox', {
-        'text-danger': props.error,
-        'custom-control-inline': props.inline,
+      className={classnames(inputClassName, 'form-check', 'checkbox', {
+        'text-danger': error,
+        'custom-control-inline': inline,
       })}
     >
       <label htmlFor={props.id} className="form-check-label">
         <input
-          checked={props.value}
+          checked={value}
           className="form-check-input"
-          disabled={props.disabled}
+          disabled={disabled}
           id={props.id}
-          name={props.name}
-          onChange={() => props.onChange(!props.value)}
-          ref={props.inputRef}
+          name={name}
+          onChange={() => onChange(!value)}
+          ref={inputRef}
           type="checkbox"
         />
         &nbsp;
