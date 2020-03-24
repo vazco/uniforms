77663116d85dc223e6cd7f2aff6515751aefbd4c
Konrad Bosak <konradbosak@protonmail.com>
2020-03-24 17:21:32 +0100

    Fixed ListField in uniforms-material.

--- packages/uniforms-material/src/ListAddField.tsx
+++ packages/uniforms-material/src/ListAddField.tsx
@@ -1,43 +1,48 @@
-import FormControl from '@material-ui/core/FormControl';
-import IconButton from '@material-ui/core/IconButton';
+import FormControl, { FormControlProps } from '@material-ui/core/FormControl';
+import IconButton, { IconButtonProps } from '@material-ui/core/IconButton';
 import React from 'react';
 import cloneDeep from 'lodash/cloneDeep';
-import { connectField, filterDOMProps } from 'uniforms';
+import { connectField, filterDOMProps, joinName, useField } from 'uniforms';
 
-const ListAdd = ({
-  disabled,
-  fullWidth,
-  icon,
-  margin,
-  parent,
-  value,
-  variant,
-  ...props
-}: {
-  disabled?: boolean;
-  parent?: any;
-  value?: any;
+type ListAddProps<T> = {
+  initialCount?: number;
   name: string;
-  [key: string]: any;
-}) => {
-  const limitNotReached =
-    !disabled && !(parent.maxCount <= parent.value.length);
+  parent?: any;
+  icon?: any;
+  value?: T;
+} & IconButtonProps &
+  Pick<FormControlProps, 'fullWidth' | 'margin' | 'variant'>;
 
+function ListAdd<T>(rawProps: ListAddProps<T>) {
+  const props = useField<ListAddProps<T>, T>(rawProps.name, rawProps, {
+    initialValue: false,
+  })[0];
+
+  const parentName = joinName(joinName(null, props.name).slice(0, -1));
+  const parent = useField<{ maxCount?: number }, T[]>(parentName, {})[0];
+  if (rawProps.parent) Object.assign(parent, rawProps.parent);
+
+  const limitNotReached =
+    !props.disabled && !(parent.maxCount! <= parent.value!.length);
   return (
-    <FormControl fullWidth={!!fullWidth} margin={margin} variant={variant}>
+    <FormControl
+      fullWidth={rawProps.fullWidth}
+      margin={rawProps.margin}
+      variant={rawProps.variant}
+    >
       <IconButton
         disabled={!limitNotReached}
-        onClick={() =>
-          limitNotReached &&
-          parent.onChange(parent.value.concat([cloneDeep(value)]))
-        }
+        onClick={() => {
+          if (limitNotReached)
+            parent.onChange(parent.value!.concat([cloneDeep(props.value!)]));
+        }}
         {...filterDOMProps(props)}
       >
-        {icon}
+        {rawProps.icon}
       </IconButton>
     </FormControl>
   );
-};
+}
 
 ListAdd.defaultProps = {
   fullWidth: true,
@@ -45,4 +50,4 @@ ListAdd.defaultProps = {
   margin: 'dense',
 };
 
-export default connectField(ListAdd, { initialValue: false });
+export default ListAdd;
--- packages/uniforms-material/src/ListItemField.tsx
+++ packages/uniforms-material/src/ListItemField.tsx
@@ -1,27 +1,34 @@
-import ListItemMaterial from '@material-ui/core/ListItem';
-import React, { Children } from 'react';
+import ListItemMaterial, {
+  ListItemProps as ListItemMaterialProps,
+} from '@material-ui/core/ListItem';
+import React, { Children, ReactNode } from 'react';
 import { connectField, joinName } from 'uniforms';
 
 import ListDelField from './ListDelField';
 import AutoField from './AutoField';
+import { AutoFieldProps } from './Types';
+
+type ListItemProps = {
+  children?: ReactNode;
+  removeIcon?: any;
+} & AutoFieldProps &
+  Pick<ListItemMaterialProps, 'dense' | 'divider' | 'disableGutters'>;
 
 const ListItem = ({
   dense,
+  children,
   divider,
   disableGutters,
   removeIcon,
   ...props
-}: {
-  name: string;
-  [key: string]: any;
-}) => (
+}: ListItemProps) => (
   <ListItemMaterial
     dense={dense}
     divider={divider}
     disableGutters={disableGutters}
   >
-    {props.children ? (
-      Children.map(props.children, child =>
+    {children ? (
+      Children.map(children as JSX.Element, child =>
         React.cloneElement(child, {
           name: joinName(props.name, child.props.name),
           label: null,
