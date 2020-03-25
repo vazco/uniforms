24f508268760c0a4b5489f002eee588d454dd495
Marta Chrzanowska <marta97chrzanowska@gmail.com>
2020-03-25 11:48:27 +0100

    Improved SelectField in uniforms-semantic.

--- packages/uniforms-semantic/src/SelectField.tsx
+++ packages/uniforms-semantic/src/SelectField.tsx
@@ -17,105 +17,24 @@ const xor = (item, array) => {
   return array.slice(0, index).concat(array.slice(index + 1));
 };
 
-type renderProps = {
+type SelectFieldProps = {
   allowedValues?: string[];
+  checkboxes?: boolean;
   disabled: boolean;
-  fieldType?: unknown;
+  error?: unknown;
+  errorMessage?: string;
+  fieldType: unknown;
   id: string;
+  inputRef?: Ref<HTMLSelectElement>;
+  label: string;
   name: string;
   onChange: (value?: string | string[]) => void;
+  placeholder: string;
+  showInlineError?: boolean;
+  required?: boolean;
   transform?: (value?: string) => string;
   value?: string | string[];
 } & HTMLProps<HTMLDivElement>;
-
-type renderCheckboxesProps = renderProps;
-
-type renderSelectProps = {
-  inputRef?: Ref<HTMLSelectElement>;
-  label: string;
-  placeholder: string;
-  required?: boolean;
-} & renderProps;
-
-type SelectFieldProps = {
-  checkboxes?: boolean;
-  error: unknown;
-  errorMessage: string;
-  inputRef?: Ref<HTMLSelectElement>;
-  label: string;
-  placeholder: string;
-  required?: boolean;
-  showInlineError: boolean;
-} & (renderCheckboxesProps | renderSelectProps);
-
-const renderCheckboxes = ({
-  allowedValues,
-  disabled,
-  fieldType,
-  id,
-  name,
-  onChange,
-  transform,
-  value,
-}: renderCheckboxesProps) =>
-  allowedValues?.map(item => (
-    <div className="field" key={item}>
-      <div className="ui checkbox">
-        <input
-          checked={fieldType === Array ? value?.includes(item) : value === item}
-          disabled={disabled}
-          id={`${id}-${escape(item)}`}
-          name={name}
-          onChange={() =>
-            onChange(fieldType === Array ? xor(item, value) : item)
-          }
-          type="checkbox"
-        />
-
-        <label htmlFor={`${id}-${escape(item)}`}>
-          {transform ? transform(item) : item}
-        </label>
-      </div>
-    </div>
-  ));
-
-const renderSelect = ({
-  allowedValues,
-  disabled,
-  id,
-  inputRef,
-  label,
-  name,
-  onChange,
-  placeholder,
-  required,
-  transform,
-  value,
-}: renderSelectProps) => (
-  <select
-    className="ui selection dropdown"
-    disabled={disabled}
-    id={id}
-    name={name}
-    onChange={event =>
-      onChange(event.target.value !== '' ? event.target.value : undefined)
-    }
-    ref={inputRef}
-    value={value ?? ''}
-  >
-    {(!!placeholder || !required || value === undefined) && (
-      <option value="" disabled={required} hidden={required}>
-        {placeholder || label}
-      </option>
-    )}
-
-    {allowedValues?.map(value => (
-      <option key={value} value={value}>
-        {transform ? transform(value) : value}
-      </option>
-    ))}
-  </select>
-);
 
 const Select = ({
   allowedValues,
@@ -144,30 +63,54 @@ const Select = ({
     {label && <label htmlFor={id}>{label}</label>}
 
     {/* TODO: Better handling of these props. */}
-    {checkboxes || fieldType === Array
-      ? renderCheckboxes({
-          allowedValues,
-          disabled,
-          id,
-          name,
-          onChange,
-          transform,
-          value,
-          fieldType,
-        })
-      : renderSelect({
-          allowedValues,
-          disabled,
-          id,
-          name,
-          onChange,
-          transform,
-          value,
-          inputRef,
-          label,
-          placeholder,
-          required,
-        })}
+    {checkboxes || fieldType === Array ? (
+      allowedValues?.map(item => (
+        <div className="field" key={item}>
+          <div className="ui checkbox">
+            <input
+              checked={
+                fieldType === Array ? value?.includes(item) : value === item
+              }
+              disabled={disabled}
+              id={`${id}-${escape(item)}`}
+              name={name}
+              onChange={() =>
+                onChange(fieldType === Array ? xor(item, value) : item)
+              }
+              type="checkbox"
+            />
+
+            <label htmlFor={`${id}-${escape(item)}`}>
+              {transform ? transform(item) : item}
+            </label>
+          </div>
+        </div>
+      ))
+    ) : (
+      <select
+        className="ui selection dropdown"
+        disabled={disabled}
+        id={id}
+        name={name}
+        onChange={event =>
+          onChange(event.target.value !== '' ? event.target.value : undefined)
+        }
+        ref={inputRef}
+        value={value ?? ''}
+      >
+        {(!!placeholder || !required || value === undefined) && (
+          <option value="" disabled={required} hidden={required}>
+            {placeholder || label}
+          </option>
+        )}
+
+        {allowedValues?.map(value => (
+          <option key={value} value={value}>
+            {transform ? transform(value) : value}
+          </option>
+        ))}
+      </select>
+    )}
 
     {!!(error && showInlineError) && (
       <div className="ui red basic pointing label">{errorMessage}</div>
