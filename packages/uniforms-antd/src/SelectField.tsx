4a77b97cb3e1ded718c4bc237dc5a0e8de561f5e
Konrad Bosak <konradbosak@protonmail.com>
2020-03-29 13:52:24 +0200

    Added SelectField types in uniforms-antd.

--- packages/uniforms-antd/src/SelectField.tsx
+++ packages/uniforms-antd/src/SelectField.tsx
@@ -1,35 +1,60 @@
-import Checkbox from 'antd/lib/checkbox';
-import Radio from 'antd/lib/radio';
+import { CheckboxGroupProps } from 'antd/lib/checkbox';
+import { RadioGroupProps } from 'antd/lib/radio';
 import React from 'react';
 import SelectAntDesign from 'antd/lib/select';
 import { connectField, filterDOMProps } from 'uniforms';
+import RadioGroup from 'antd/lib/radio/group';
+import CheckboxGroup from 'antd/lib/checkbox/Group';
+import { SelectInputProps } from '@material-ui/core/Select/SelectInput';
 
 import wrapField from './wrapField';
 
 // SelectAntD does not recognize prop 'name'
 const SelectAntD: any = SelectAntDesign;
 
-const renderCheckboxes = props =>
-  React.createElement(
-    (props.fieldType === Array ? Checkbox : Radio).Group as any,
-    {
-      disabled: props.disabled,
-      id: props.id,
-      name: props.name,
-      onChange:
-        props.fieldType === Array
-          ? value => props.onChange(value)
-          : event => props.onChange(event.target.value),
-      options: props.allowedValues.map(value => ({
+type CheckboxesProps = {
+  fieldType?: typeof Array | any;
+  onChange: (
+    value?: string | boolean[] | number | { [key: string]: any },
+  ) => void;
+  transform?: (value?: string) => string;
+  allowedValues: string[];
+  id: string;
+} & (CheckboxGroupProps | RadioGroupProps);
+
+const renderCheckboxes = (props: CheckboxesProps) => {
+  const Group = props.fieldType === Array ? CheckboxGroup : RadioGroup;
+  const checkboxProps = {
+    disabled: props.disabled,
+    id: props.id,
+    name: props.name,
+    onChange:
+      props.fieldType === Array
+        ? value => props.onChange && props.onChange(value)
+        : event => props.onChange && props.onChange(event.target.value),
+    options: props.allowedValues.map(value => {
+      return {
         label: props.transform ? props.transform(value) : value,
         value,
-      })),
-      value: props.value,
-      ...filterDOMProps(props),
-    },
-  );
+      };
+    }),
+    value: props.value,
+    ...filterDOMProps(props),
+  };
+  return <Group {...checkboxProps} />;
+};
 
-const renderSelect = props => (
+type SelectProps = {
+  required?: boolean;
+  id: string;
+  fieldType?: typeof Array | any;
+  onChange: (value?: string | string[]) => void;
+  placeholder: string;
+  allowedValues?: string[];
+  transform?: (value?: string) => string;
+} & SelectInputProps;
+
+const renderSelect = (props: SelectProps) => (
   <SelectAntD
     allowClear={!props.required}
     disabled={props.disabled}
@@ -42,7 +67,7 @@ const renderSelect = props => (
     value={props.value || (props.fieldType === Array ? [] : undefined)}
     {...filterDOMProps(props)}
   >
-    {props.allowedValues.map(value => (
+    {props.allowedValues!.map(value => (
       <SelectAntD.Option key={value} value={value}>
         {props.transform ? props.transform(value) : value}
       </SelectAntD.Option>
@@ -50,7 +75,17 @@ const renderSelect = props => (
   </SelectAntD>
 );
 
-const Select = ({ checkboxes, ...props }: any) =>
-  wrapField(props, checkboxes ? renderCheckboxes(props) : renderSelect(props));
+type SelectFieldProps = { checkboxes?: boolean } & (
+  | CheckboxesProps
+  | SelectProps
+);
+
+const Select = (props: SelectFieldProps) =>
+  wrapField(
+    props,
+    props.checkboxes
+      ? renderCheckboxes(props as CheckboxesProps)
+      : renderSelect(props as SelectProps),
+  );
 
 export default connectField(Select);
