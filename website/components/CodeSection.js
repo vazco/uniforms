40e64a673ad2b3deb81f29974bd973b42336841b
Radosław Miernik <radekmie@gmail.com>
2020-04-01 13:47:16 +0200

    Added CodeSection component.

---
+++ website/components/CodeSection.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import components from '@theme/MDXComponents';
+
+export default function CodeSection({ language, replace, section, source }) {
+  // Unwrap ES module.
+  if (typeof source === 'object' && 'default' in source) {
+    source = source.default;
+  }
+
+  // Cut out only desired section.
+  if (section) {
+    const pattern = new RegExp(
+      `^.*// <${section}>\\s(.*?)\\s// </${section}>\\s.*$`,
+      's'
+    );
+
+    source = source.replace(pattern, '$1');
+  }
+
+  // Replace all mapped things.
+  if (replace)
+    for (const [pattern, value] of Object.entries(replace))
+      source = source.replace(new RegExp(pattern, 'gs'), value);
+
+  return (
+    <components.pre>
+      <components.code
+        children={source}
+        className={`language-${language}`}
+        mdxType="code"
+        originalType="code"
+        parentName="pre"
+      />
+    </components.pre>
+  );
+}
