ba58a9afe23f75049febffcc420cc0fedafd34e8
Radosław Miernik <radekmie@gmail.com>
2020-04-01 13:53:57 +0200

    Converted few examples to use CodeSection.

--- docs/examples/Tutorial/GuestFormProfessionAdditionalInfo.js
+++ docs/examples/Tutorial/GuestFormProfessionAdditionalInfo.js
@@ -1,6 +1,4 @@
 import React from 'react';
-
-import GuestSchema2 from './GuestSchema2';
 import {
   AutoForm,
   AutoField,
@@ -8,9 +6,11 @@ import {
   SubmitField
 } from '../../../website/components/universal';
 
-export default function GuestFormProfessionAdditionalInfo() {
+import GuestSchema from './GuestSchema2';
+
+export default function GuestForm() {
   return (
-    <AutoForm schema={GuestSchema2}>
+    <AutoForm schema={GuestSchema} onSubmit={console.log}>
       <h4>IT meeting guest questionnaire</h4>
       <AutoField name="lastName" />
       <ErrorField name="lastName">
