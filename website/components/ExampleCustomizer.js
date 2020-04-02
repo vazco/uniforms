04b4385d8f0fe8540d730fe134419b7ed30e8faa
Radosław Miernik <radekmie@gmail.com>
2020-04-02 20:59:37 +0200

    Simplified ExampleCustomizer.

--- website/components/ExampleCustomizer.js
+++ website/components/ExampleCustomizer.js
@@ -5,6 +5,7 @@ import {
   Database as DatabaseIcon
 } from 'react-feather';
 
+import CodeSection from './CodeSection';
 import FormWrapper from './FormWrapper';
 import TogglerTabs from './TogglerTabs';
 import { ThemeProvider } from './ThemeContext';
@@ -29,25 +30,53 @@ const toggles = [
   { name: 'Schema', tooltipText: 'Show schema', icon: <DatabaseIcon /> }
 ];
 
-const ExampleCustomizer = ({ code, example, schema }) => (
-  <TogglerTabs group="examples" tabsItems={tabs} togglerItems={toggles}>
-    {({ tab: { value: theme }, toggle: { name } }) => {
-      switch (name) {
-        case 'Example':
-          return (
-            <ThemeProvider value={theme}>
-              <FormWrapper>{example}</FormWrapper>
-            </ThemeProvider>
-          );
-        case 'Code':
-          return code(theme);
-        case 'Schema':
-          return schema;
-        default:
-          return <p>Nothing selected</p>;
-      }
-    }}
-  </TogglerTabs>
-);
+export default function ExampleCustomizer({ code, example, schema }) {
+  function Code({ theme }) {
+    if (!('default' in code)) return code(theme);
+    return (
+      <CodeSection
+        language="js"
+        replace={{ "'[^']*?/universal'": `'uniforms-${theme}'` }}
+        source={code.default}
+      />
+    );
+  }
 
-export default ExampleCustomizer;
+  function Example({ theme }) {
+    let content;
+    if ('default' in example) {
+      const ExampleComponent = example.default;
+      content = <ExampleComponent />;
+    } else {
+      content = example;
+    }
+
+    return (
+      <ThemeProvider value={theme}>
+        <FormWrapper>{content}</FormWrapper>
+      </ThemeProvider>
+    );
+  }
+
+  function Schema() {
+    if (!('default' in schema)) return schema;
+    return <CodeSection language="js" source={schema.default} />;
+  }
+
+  return (
+    <TogglerTabs group="examples" tabsItems={tabs} togglerItems={toggles}>
+      {({ tab: { value: theme }, toggle: { name } }) => {
+        switch (name) {
+          case 'Code':
+            return <Code theme={theme} />;
+          case 'Example':
+            return <Example theme={theme} />;
+          case 'Schema':
+            return <Schema />;
+          default:
+            return null;
+        }
+      }}
+    </TogglerTabs>
+  );
+}
