92d397fe2cbde829b45d0042da6ddfedffc7e287
Monteth <konrad.bosak@vazco.eu>
2020-07-01 12:43:07 +0200

    Moved 'matchMedia' property definition to setupEnzyme

--- packages/uniforms-antd/__tests__/_mount.tsx
+++ packages/uniforms-antd/__tests__/_mount.tsx
@@ -1,20 +1,6 @@
 import { context } from 'uniforms';
 import { mount as enzyme } from 'enzyme';
 
-Object.defineProperty(window, 'matchMedia', {
-  writable: true,
-  value: jest.fn().mockImplementation(query => ({
-    matches: false,
-    media: query,
-    onchange: null,
-    addListener: jest.fn(), // deprecated
-    removeListener: jest.fn(), // deprecated
-    addEventListener: jest.fn(),
-    removeEventListener: jest.fn(),
-    dispatchEvent: jest.fn(),
-  })),
-});
-
 function mount(node, options) {
   if (options === undefined) return enzyme(node);
   return enzyme(node, {
--- scripts/setupEnzyme.ts
+++ scripts/setupEnzyme.ts
@@ -1,4 +1,18 @@
 import Adapter from 'enzyme-adapter-react-16';
 import Enzyme from 'enzyme';
 
+Object.defineProperty(window, 'matchMedia', {
+  writable: true,
+  value: jest.fn().mockImplementation(query => ({
+    matches: false,
+    media: query,
+    onchange: null,
+    addListener: jest.fn(), // deprecated
+    removeListener: jest.fn(), // deprecated
+    addEventListener: jest.fn(),
+    removeEventListener: jest.fn(),
+    dispatchEvent: jest.fn(),
+  })),
+});
+
 Enzyme.configure({ adapter: new Adapter() });
