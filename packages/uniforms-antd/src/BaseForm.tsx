d86f5a0660917f8bfa6e37ad6b94b8b324bc7f83
Monteth <konrad.bosak@vazco.eu>
2020-07-03 10:51:26 +0200

    Changed antd form layout and ListItemField margin.

--- packages/uniforms-antd/src/BaseForm.tsx
+++ packages/uniforms-antd/src/BaseForm.tsx
@@ -1,10 +1,20 @@
 import { BaseForm } from 'uniforms';
+import classnames from 'classnames';
 
 function AntD(parent: any): any {
   class _ extends parent {
     static AntD = AntD;
 
     static displayName = `AntD${parent.displayName}`;
+
+    getNativeFormProps() {
+      const { className, ...props } = super.getNativeFormProps();
+
+      return {
+        ...props,
+        className: classnames('ant-form', 'ant-form-vertical', className),
+      };
+    }
   }
 
   return _;
