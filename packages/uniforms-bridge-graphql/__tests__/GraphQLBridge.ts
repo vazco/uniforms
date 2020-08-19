cfe174d1001e81149d07570f7209331216f77e84
Radosław Miernik <radekmie@gmail.com>
2020-08-19 13:40:17 +0200

    Reworked GraphQLBridge.getField (closes #786).

--- packages/uniforms-bridge-graphql/__tests__/GraphQLBridge.ts
+++ packages/uniforms-bridge-graphql/__tests__/GraphQLBridge.ts
@@ -78,23 +78,19 @@ describe('GraphQLBridge', () => {
   const astT = buildASTSchema(parse(schemaT));
 
   const bridgeI = new GraphQLBridge(
-    astI.getType('Post') as GraphQLInputObjectType,
+    astI.getType('Post')!,
     schemaValidator,
     schemaData,
   );
   const bridgeT = new GraphQLBridge(
-    astT.getType('Post') as GraphQLObjectType,
+    astT.getType('Post')!,
     schemaValidator,
     schemaData,
   );
 
   describe('#constructor()', () => {
     it('always ensures `extras`', () => {
-      const bridge = new GraphQLBridge(
-        astI.getType('Post') as GraphQLInputObjectType,
-        schemaValidator,
-      );
-
+      const bridge = new GraphQLBridge(astI.getType('Post')!, schemaValidator);
       expect(bridge.extras).toEqual({});
     });
   });
@@ -197,10 +193,10 @@ describe('GraphQLBridge', () => {
     });
 
     it('throws on not found field', () => {
-      expect(() => bridgeI.getField('x')).toThrow(/Field not found in schema/);
-      expect(() => bridgeI.getField('author.x')).toThrow(
-        /Field not found in schema/,
-      );
+      const error = /Field not found in schema/;
+      expect(() => bridgeI.getField('x')).toThrow(error);
+      expect(() => bridgeI.getField('author.x')).toThrow(error);
+      expect(() => bridgeI.getField('author.tags.x')).toThrow(error);
     });
   });
 
