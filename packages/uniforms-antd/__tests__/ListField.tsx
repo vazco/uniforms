489c9ee7c72ee2e2077de87796f682f3b0a83921
Konrad Bosak <konrad.bosak@vazco.eu>
2021-02-10 15:08:21 +0100

    Added test for specified style prop in ListField, uniforms-antd.

--- packages/uniforms-antd/__tests__/ListField.tsx
+++ packages/uniforms-antd/__tests__/ListField.tsx
@@ -144,3 +144,19 @@ test('<ListField> - renders correct error style', () => {
     'rgb(255, 85, 0)',
   );
 });
+
+test('<ListField> - renders correct error style (with specified style prop)', () => {
+  const error = new Error();
+  const element = (
+    <ListField name="x" error={error} style={{ marginLeft: '8px' }} />
+  );
+  const wrapper = mount(
+    element,
+    createContext({ x: { type: Array }, 'x.$': { type: String } }),
+  );
+
+  expect(wrapper.find('div').at(0).prop('style')).toHaveProperty(
+    'marginLeft',
+    '8px',
+  );
+});
