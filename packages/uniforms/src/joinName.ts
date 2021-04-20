044f8d5ab09d7418caae8f4a992e34b922fe80bb
Radosław Miernik <radekmie@gmail.com>
2021-04-20 14:31:16 +0200

    Optimized joinName.

--- packages/uniforms/src/joinName.ts
+++ packages/uniforms/src/joinName.ts
@@ -1,13 +1,23 @@
 export function joinName(flag: null, ...parts: unknown[]): string[];
 export function joinName(...parts: unknown[]): string;
 export function joinName(...parts: unknown[]) {
-  const name = parts.reduce(
-    (parts: unknown[], part: unknown) =>
-      part || part === 0
-        ? parts.concat(typeof part === 'string' ? part.split('.') : part)
-        : parts,
-    [],
-  );
+  const name: string[] = [];
+  for (let index = 0; index !== parts.length; ++index) {
+    const part = parts[index];
+    if (part || part === 0) {
+      if (typeof part === 'string') {
+        if (part.indexOf('.') !== -1) {
+          name.push(...part.split('.'));
+        } else {
+          name.push(part);
+        }
+      } else if (Array.isArray(part)) {
+        parts.splice(index--, 1, ...part);
+      } else {
+        name.push('' + part);
+      }
+    }
+  }
 
-  return parts[0] === null ? name.map(part => '' + part) : name.join('.');
+  return parts[0] === null ? name : name.join('.');
 }
