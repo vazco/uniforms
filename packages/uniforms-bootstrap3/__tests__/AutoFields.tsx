6b1e3b25792b1184c14092c22fc5541b48ec19ae
Konrad Bosak <konrad.bosak@vazco.eu>
2021-04-20 16:04:48 +0200

    Added showInlineError prop to AutoFields (closes #827).

--- packages/uniforms-bootstrap3/__tests__/AutoFields.tsx
+++ packages/uniforms-bootstrap3/__tests__/AutoFields.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { AutoFields } from 'uniforms-bootstrap3';
+import { AutoFields, AutoField } from 'uniforms-bootstrap3';
 
 import createContext from './_createContext';
 import mount from './_mount';
@@ -85,4 +85,22 @@ test('<AutoFields> - wraps fields in specified element', () => {
   );
 
   expect(wrapper.find('section').find('input')).toHaveLength(3);
+});
+
+test('<AutoFields> - pass props to the children', () => {
+  const element = <AutoFields showInlineError />;
+  const wrapper = mount(
+    element,
+    createContext({
+      x: { type: String },
+      y: { type: String },
+      z: { type: String },
+    }),
+  );
+
+  const hasShowInlineErrorMap = wrapper
+    .find(AutoField)
+    .map(node => node.prop('showInlineError'));
+  expect(hasShowInlineErrorMap).toHaveLength(3);
+  expect(hasShowInlineErrorMap).toBeTruthy();
 });
