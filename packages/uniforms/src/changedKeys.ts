ca2da5bd806c8a2091f46c86b47e65c24d3cd55e
Radosław Miernik <radekmie@gmail.com>
2021-05-10 21:46:53 +0200

    Optimized changedKeys.

--- packages/uniforms/src/changedKeys.ts
+++ packages/uniforms/src/changedKeys.ts
@@ -1,24 +1,36 @@
 import isEqual from 'lodash/isEqual';
-import xorWith from 'lodash/xorWith';
 
 import { joinName } from './joinName';
 
+// eslint-disable-next-line complexity
 export function changedKeys<T>(root: string, valueA?: T, valueB?: T) {
   if (!valueA || valueA !== Object(valueA) || valueA instanceof Date) {
     return isEqual(valueA, valueB) ? [] : [root];
   }
-  if (!valueB) {
-    return [root, ...Object.keys(valueA).map(key => joinName(root, key))];
+
+  const changed = [root];
+  if (valueB) {
+    for (const key in valueA) {
+      if (!(key in valueB) || !isEqual(valueA[key], valueB[key])) {
+        changed.push(joinName(root, key));
+      }
+    }
+
+    for (const key in valueB) {
+      if (!(key in valueA)) {
+        changed.push(joinName(root, key));
+      }
+    }
+
+    if (changed.length === 1) {
+      changed.pop();
+    }
+  } else {
+    // eslint-disable-next-line guard-for-in
+    for (const key in valueA) {
+      changed.push(joinName(root, key));
+    }
   }
 
-  const changed = xorWith(
-    Object.entries(valueA),
-    Object.entries(valueB),
-    isEqual,
-  ).map(pair => joinName(root, pair[0]));
-
-  if (changed.length) {
-    changed.unshift(root);
-  }
   return changed;
 }
