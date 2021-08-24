0901598c731533ce1c3bd2e639f013dd1fb8bb90
wadamek65 <wadamek65@gmail.com>
2021-08-24 16:09:23 +0200

    Removed redundant casts.

--- packages/uniforms-bootstrap3/src/ValidatedQuickForm.tsx
+++ packages/uniforms-bootstrap3/src/ValidatedQuickForm.tsx
@@ -1,9 +1,5 @@
-import { ValidatedQuickForm } from 'uniforms';
-
 import BaseForm from './BaseForm';
 import QuickForm from './QuickForm';
 import ValidatedForm from './ValidatedForm';
 
-export default ValidatedForm.Validated(
-  QuickForm.Quick(BaseForm),
-) as ValidatedQuickForm;
+export default ValidatedForm.Validated(QuickForm.Quick(BaseForm));
