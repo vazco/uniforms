f03b63770ffff16da922ae741a17d40d8daff887
wadamek65 <wadamek65@gmail.com>
2021-08-24 15:23:33 +0200

    Added explicit form type casting (except BaseForm).

--- packages/uniforms-material/src/ValidatedQuickForm.tsx
+++ packages/uniforms-material/src/ValidatedQuickForm.tsx
@@ -1,5 +1,9 @@
+import { ValidatedQuickForm } from 'uniforms';
+
 import BaseForm from './BaseForm';
 import QuickForm from './QuickForm';
 import ValidatedForm from './ValidatedForm';
 
-export default ValidatedForm.Validated(QuickForm.Quick(BaseForm));
+export default ValidatedForm.Validated(
+  QuickForm.Quick(BaseForm),
+) as typeof ValidatedQuickForm;
