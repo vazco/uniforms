4d2e0cff06ddb387edcf9efa8031232f5c03707f
sabativi <victor.sabatier@gmail.com>
2021-11-25 16:23:17 +0100

    WIP

--- packages/uniforms-material/__tests__/_createSchema.ts
+++
@@ -1,6 +0,0 @@
-import SimpleSchema from 'simpl-schema';
-import { SimpleSchema2Bridge } from 'uniforms-bridge-simple-schema-2';
-
-export default function createSchema(schema = {}) {
-  return new SimpleSchema2Bridge(new SimpleSchema(schema));
-}
