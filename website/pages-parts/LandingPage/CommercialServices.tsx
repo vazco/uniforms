9fee2a260ece6075a78abff70c77988a265b7246
Piotr Pośpiech <ptkpospiech@gmail.com>
2022-03-18 11:17:59 +0100

    added commas

--- website/pages-parts/LandingPage/CommercialServices.tsx
+++ website/pages-parts/LandingPage/CommercialServices.tsx
@@ -12,7 +12,7 @@ export function CommercialServices() {
       className={classNames(
         'hero hero--primary',
         styles['overflow-hidden'],
-        styles.whyus
+        styles.whyus,
       )}
     >
       <div className="container">
@@ -33,7 +33,7 @@ export function CommercialServices() {
                 className={classNames(
                   styles.text,
                   styles.bullets,
-                  styles['commercial-bullets']
+                  styles['commercial-bullets'],
                 )}
               >
                 <li>
