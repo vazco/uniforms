9e4362cd82dcec89db559b43d179054e4bee1579
Radosław Miernik <radekmie@gmail.com>
2022-09-27 14:28:41 +0200

    Added ZodBridge (step 3).

--- packages/uniforms-bridge-zod/__tests__/ZodBridge.ts
+++ packages/uniforms-bridge-zod/__tests__/ZodBridge.ts
@@ -1,7 +1,38 @@
 import { ZodBridge } from 'uniforms-bridge-zod';
-import { number, object, string } from 'zod';
+import { array, number, object, string } from 'zod';
 
 describe('ZodBridge', () => {
+  describe('#getField', () => {
+    it('works with root schema', () => {
+      const schema = object({});
+      const bridge = new ZodBridge(schema);
+      expect(bridge.getField('')).toBe(schema);
+    });
+
+    it('works with simple types', () => {
+      const schema = object({ a: string(), b: number() });
+      const bridge = new ZodBridge(schema);
+      expect(bridge.getField('a')).toBe(schema.shape.a);
+      expect(bridge.getField('b')).toBe(schema.shape.b);
+    });
+
+    it('works with arrays', () => {
+      const schema = object({ a: array(array(string())) });
+      const bridge = new ZodBridge(schema);
+      expect(bridge.getField('a')).toEqual(schema.shape.a);
+      expect(bridge.getField('a.$')).toEqual(schema.shape.a.element);
+      expect(bridge.getField('a.$.$')).toEqual(schema.shape.a.element.element);
+    });
+
+    it('works with nested objects', () => {
+      const schema = object({ a: object({ b: object({ c: string() }) }) });
+      const bridge = new ZodBridge(schema);
+      expect(bridge.getField('a')).toBe(schema.shape.a);
+      expect(bridge.getField('a.b')).toBe(schema.shape.a.shape.b);
+      expect(bridge.getField('a.b.c')).toBe(schema.shape.a.shape.b.shape.c);
+    });
+  });
+
   describe('#getSubfields', () => {
     it('works with empty objects', () => {
       const schema = object({});
@@ -13,6 +44,29 @@ describe('ZodBridge', () => {
       const schema = object({ a: string(), b: number() });
       const bridge = new ZodBridge(schema);
       expect(bridge.getSubfields()).toEqual(['a', 'b']);
+    });
+
+    it('works with simple types', () => {
+      const schema = object({ a: string(), b: number() });
+      const bridge = new ZodBridge(schema);
+      expect(bridge.getSubfields('a')).toEqual([]);
+      expect(bridge.getSubfields('b')).toEqual([]);
+    });
+
+    it('works with arrays', () => {
+      const schema = object({ a: array(array(string())) });
+      const bridge = new ZodBridge(schema);
+      expect(bridge.getSubfields('a')).toEqual(['$']);
+      expect(bridge.getSubfields('a.$')).toEqual(['$']);
+      expect(bridge.getSubfields('a.$.$')).toEqual([]);
+    });
+
+    it('works with nested objects', () => {
+      const schema = object({ a: object({ b: object({ c: string() }) }) });
+      const bridge = new ZodBridge(schema);
+      expect(bridge.getSubfields('a')).toEqual(['b']);
+      expect(bridge.getSubfields('a.b')).toEqual(['c']);
+      expect(bridge.getSubfields('a.b.c')).toEqual([]);
     });
   });
 });
--- packages/uniforms-bridge-zod/src/ZodBridge.ts
+++ packages/uniforms-bridge-zod/src/ZodBridge.ts
@@ -1,20 +1,45 @@
+import invariant from 'invariant';
 import memoize from 'lodash/memoize';
-import { Bridge } from 'uniforms';
-import { ZodObject, ZodRawShape } from 'zod';
+import { Bridge, joinName } from 'uniforms';
+import { ZodArray, ZodObject, ZodRawShape, ZodType } from 'zod';
+
+function fieldInvariant(name: string, condition: boolean): asserts condition {
+  invariant(condition, 'Field not found in schema: "%s"', name);
+}
 
 export default class ZodBridge<T extends ZodRawShape> extends Bridge {
   constructor(public schema: ZodObject<T>) {
     super();
 
+    this.getField = memoize(this.getField.bind(this));
     this.getSubfields = memoize(this.getSubfields.bind(this));
   }
 
-  getSubfields(name?: string): string[] {
-    if (name === undefined) {
-      return Object.keys(this.schema.shape);
+  getField(name: string) {
+    let field: ZodType = this.schema;
+    for (const key of joinName(null, name)) {
+      if (key === '$' || key === '' + parseInt(key, 10)) {
+        fieldInvariant(name, field instanceof ZodArray);
+        field = field.element;
+      } else {
+        fieldInvariant(name, field instanceof ZodObject);
+        field = field.shape[joinName.unescape(key)];
+      }
     }
 
-    // TODO.
+    return field;
+  }
+
+  getSubfields(name = '') {
+    const field = this.getField(name);
+    if (field instanceof ZodArray) {
+      return ['$'];
+    }
+
+    if (field instanceof ZodObject) {
+      return Object.keys(field.shape);
+    }
+
     return [];
   }
 }
