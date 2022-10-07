b65e0297e6d2a1b943e8f815789793ee590e7939
Wojciech Adamek <36732892+wadamek65@users.noreply.github.com>
2022-10-07 14:39:18 +0200

    Unified implementation of DateField type handling (fixes #1138, #1162).

--- packages/uniforms-bootstrap3/src/DateField.tsx
+++ packages/uniforms-bootstrap3/src/DateField.tsx
@@ -4,9 +4,12 @@ import { connectField, HTMLFieldProps } from 'uniforms';
 
 import wrapField from './wrapField';
 
+type DateFieldType = 'date' | 'datetime-local';
+
 /* istanbul ignore next */
 const DateConstructor = (typeof global === 'object' ? global : window).Date;
-const dateFormat = (value?: Date) => value?.toISOString().slice(0, -8);
+const dateFormat = (value?: Date, type: DateFieldType = 'datetime-local') =>
+  value?.toISOString().slice(0, type === 'datetime-local' ? -8 : -14);
 
 export type DateFieldProps = HTMLFieldProps<
   Date,
@@ -18,7 +21,7 @@ export type DateFieldProps = HTMLFieldProps<
     max?: Date;
     min?: Date;
     wrapClassName?: string;
-    type?: 'date' | 'datetime-local';
+    type?: DateFieldType;
   }
 >;
 
@@ -35,11 +38,9 @@ function Date({
   placeholder,
   readOnly,
   value,
-  type,
+  type = 'datetime-local',
   ...props
 }: DateFieldProps) {
-  const dateType = type === 'date' ? type : 'datetime-local';
-
   return wrapField(
     { ...props, id },
     <input
@@ -62,8 +63,8 @@ function Date({
       placeholder={placeholder}
       readOnly={readOnly}
       ref={inputRef}
-      type={dateType}
-      value={dateFormat(value) ?? ''}
+      type={type}
+      value={dateFormat(value, type) ?? ''}
     />,
   );
 }
