b781eef12ba311fd1f80870e32375940559cb69b
Ernest Teluk <teluk.ernest@gmail.com>
2022-11-28 18:28:19 +0100

    fix test to it name in material tests

--- packages/uniforms-material/__tests__/index.ts
+++ packages/uniforms-material/__tests__/index.ts
@@ -1,6 +1,6 @@
 import * as material from 'uniforms-material';
 
-test('exports everything', () => {
+it('exports everything', () => {
   expect(material).toEqual({
     AutoFields: expect.any(Function),
     AutoField: expect.any(Function),
