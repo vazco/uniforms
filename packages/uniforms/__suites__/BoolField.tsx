1ffdc9bbfe455bd39baa3abf50eafd0a3d8cb03d
Piotr Pośpiech <ptkpospiech@gmail.com>
2023-06-09 16:37:42 +0200

    added skipTestIf

--- packages/uniforms/__suites__/BoolField.tsx
+++ packages/uniforms/__suites__/BoolField.tsx
@@ -5,6 +5,7 @@ import React, { ComponentType } from 'react';
 import z from 'zod';
 
 import { renderWithZod } from './render-zod';
+import { skipTestIf } from './skipTestIf';
 
 // eslint-disable-next-line complexity
 export function testBoolField(
@@ -31,37 +32,33 @@ export function testBoolField(
     expect(element).toHaveAttribute('name', 'x');
   });
 
-  if (options?.testCheckbox) {
-    test('<BoolField> - renders a checkbox', () => {
-      renderWithZod({
-        element: <BoolField name="x" data-testid="boolfield" checkbox />,
-        schema: z.object({ x: z.boolean() }),
-      });
-
-      const element = options?.isButton
-        ? screen.getByTestId('boolfield')
-        : screen.getByTestId('boolfield').querySelector('input');
-
-      expect(element).toHaveAttribute('name', 'x');
+  skipTestIf(!options?.testCheckbox)('<BoolField> - renders a checkbox', () => {
+    renderWithZod({
+      element: <BoolField name="x" data-testid="boolfield" checkbox />,
+      schema: z.object({ x: z.boolean() }),
     });
-  }
 
-  if (options?.testSwitch) {
-    test('<BoolField> - renders a switch', () => {
-      renderWithZod({
-        element: (
-          <BoolField name="x" data-testid="boolfield" appearance="switch" />
-        ),
-        schema: z.object({ x: z.boolean() }),
-      });
+    const element = options?.isButton
+      ? screen.getByTestId('boolfield')
+      : screen.getByTestId('boolfield').querySelector('input');
 
-      const element = options?.isButton
-        ? screen.getByTestId('boolfield')
-        : screen.getByTestId('boolfield').querySelector('input');
+    expect(element).toHaveAttribute('name', 'x');
+  });
 
-      expect(element).toHaveAttribute('name', 'x');
+  skipTestIf(!options?.testSwitch)('<BoolField> - renders a switch', () => {
+    renderWithZod({
+      element: (
+        <BoolField name="x" data-testid="boolfield" appearance="switch" />
+      ),
+      schema: z.object({ x: z.boolean() }),
     });
-  }
+
+    const element = options?.isButton
+      ? screen.getByTestId('boolfield')
+      : screen.getByTestId('boolfield').querySelector('input');
+
+    expect(element).toHaveAttribute('name', 'x');
+  });
 
   test('<BoolField> - renders an input with correct id (inherited)', () => {
     renderWithZod({
@@ -76,8 +73,9 @@ export function testBoolField(
     expect(element).toBeTruthy();
   });
 
-  if (options?.testCheckbox) {
-    test('<BoolField> - renders a checkbox with correct id (inherited)', () => {
+  skipTestIf(!options?.testCheckbox)(
+    '<BoolField> - renders a checkbox with correct id (inherited)',
+    () => {
       renderWithZod({
         element: <BoolField name="x" data-testid="boolfield" checkbox />,
         schema: z.object({ x: z.boolean() }),
@@ -88,11 +86,12 @@ export function testBoolField(
         : screen.getByTestId('boolfield').querySelector('input');
 
       expect(element).toBeTruthy();
-    });
-  }
+    },
+  );
 
-  if (options?.testSwitch) {
-    test('<BoolField> - renders a switch with correct id (inherited)', () => {
+  skipTestIf(!options?.testSwitch)(
+    '<BoolField> - renders a switch with correct id (inherited)',
+    () => {
       renderWithZod({
         element: (
           <BoolField name="x" data-testid="boolfield" appearance="switch" />
@@ -105,8 +104,8 @@ export function testBoolField(
         : screen.getByTestId('boolfield').querySelector('input');
 
       expect(element).toBeTruthy();
-    });
-  }
+    },
+  );
 
   test('<BoolField> - renders an input with correct id (specified)', () => {
     renderWithZod({
@@ -121,8 +120,9 @@ export function testBoolField(
     expect(element).toHaveAttribute('id', 'y');
   });
 
-  if (options?.testCheckbox) {
-    test('<BoolField> - renders a checkbox with correct id (specified)', () => {
+  skipTestIf(!options?.testCheckbox)(
+    '<BoolField> - renders a checkbox with correct id (specified)',
+    () => {
       renderWithZod({
         element: <BoolField name="x" data-testid="boolfield" id="y" checkbox />,
         schema: z.object({ x: z.boolean() }),
@@ -133,11 +133,12 @@ export function testBoolField(
         : screen.getByTestId('boolfield').querySelector('input');
 
       expect(element).toHaveAttribute('id', 'y');
-    });
-  }
+    },
+  );
 
-  if (options?.testSwitch) {
-    test('<BoolField> - renders a switch with correct id (specified)', () => {
+  skipTestIf(!options?.testSwitch)(
+    '<BoolField> - renders a switch with correct id (specified)',
+    () => {
       renderWithZod({
         element: (
           <BoolField
@@ -155,8 +156,8 @@ export function testBoolField(
         : screen.getByTestId('boolfield').querySelector('input');
 
       expect(element).toHaveAttribute('id', 'y');
-    });
-  }
+    },
+  );
 
   test('<BoolField> - renders an input with correct name', () => {
     renderWithZod({
@@ -171,8 +172,9 @@ export function testBoolField(
     expect(element).toHaveAttribute('name', 'x');
   });
 
-  if (options?.testSwitch) {
-    test('<BoolField> - renders a switch with correct name', () => {
+  skipTestIf(!options?.testSwitch)(
+    '<BoolField> - renders a switch with correct name',
+    () => {
       renderWithZod({
         element: (
           <BoolField name="x" data-testid="boolfield" appearance="switch" />
@@ -185,11 +187,12 @@ export function testBoolField(
         : screen.getByTestId('boolfield').querySelector('input');
 
       expect(element).toHaveAttribute('name', 'x');
-    });
-  }
+    },
+  );
 
-  if (options?.testCheckbox) {
-    test('<BoolField> - renders a checkbox with correct name', () => {
+  skipTestIf(!options?.testCheckbox)(
+    '<BoolField> - renders a checkbox with correct name',
+    () => {
       renderWithZod({
         element: <BoolField name="x" data-testid="boolfield" checkbox />,
         schema: z.object({ x: z.boolean() }),
@@ -200,8 +203,8 @@ export function testBoolField(
         : screen.getByTestId('boolfield').querySelector('input');
 
       expect(element).toHaveAttribute('name', 'x');
-    });
-  }
+    },
+  );
 
   test('<BoolField> - renders an input with correct type', () => {
     renderWithZod({
@@ -219,8 +222,9 @@ export function testBoolField(
     );
   });
 
-  if (options?.testCheckbox) {
-    test('<BoolField> - renders a checkbox with correct type', () => {
+  skipTestIf(!options?.testCheckbox)(
+    '<BoolField> - renders a checkbox with correct type',
+    () => {
       renderWithZod({
         element: <BoolField name="x" data-testid="boolfield" checkbox />,
         schema: z.object({ x: z.boolean() }),
@@ -231,11 +235,12 @@ export function testBoolField(
         : screen.getByTestId('boolfield').querySelector('input');
 
       expect(element).toHaveAttribute('type', 'checkbox');
-    });
-  }
+    },
+  );
 
-  if (options?.testSwitch) {
-    test('<BoolField> - renders a switch with correct type', () => {
+  skipTestIf(!options?.testSwitch)(
+    '<BoolField> - renders a switch with correct type',
+    () => {
       renderWithZod({
         element: (
           <BoolField name="x" data-testid="boolfield" appearance="switch" />
@@ -248,8 +253,8 @@ export function testBoolField(
         : screen.getByTestId('boolfield').querySelector('input');
 
       expect(element).toHaveAttribute('type', 'checkbox');
-    });
-  }
+    },
+  );
 
   test('<BoolField> - renders an input with correct disabled state', () => {
     renderWithZod({
@@ -264,8 +269,9 @@ export function testBoolField(
     expect(element).toBeDisabled();
   });
 
-  if (options?.testCheckbox) {
-    test('<BoolField> - renders a checkbox with correct disabled state', () => {
+  skipTestIf(!options?.testCheckbox)(
+    '<BoolField> - renders a checkbox with correct disabled state',
+    () => {
       renderWithZod({
         element: (
           <BoolField name="x" data-testid="boolfield" disabled checkbox />
@@ -278,11 +284,12 @@ export function testBoolField(
         : screen.getByTestId('boolfield').querySelector('input');
 
       expect(element).toBeDisabled();
-    });
-  }
+    },
+  );
 
-  if (options?.testSwitch) {
-    test('<BoolField> - renders a switch with correct disabled state', () => {
+  skipTestIf(!options?.testSwitch)(
+    '<BoolField> - renders a switch with correct disabled state',
+    () => {
       renderWithZod({
         element: (
           <BoolField
@@ -300,8 +307,8 @@ export function testBoolField(
         : screen.getByTestId('boolfield').querySelector('input');
 
       expect(element).toBeDisabled();
-    });
-  }
+    },
+  );
 
   test('<BoolField> - renders an input with correct readOnly state', async () => {
     const onChange = jest.fn();
@@ -322,8 +329,9 @@ export function testBoolField(
     expect(onChange).not.toHaveBeenCalled();
   });
 
-  if (options?.testCheckbox) {
-    test('<BoolField> - renders a checkbox with correct readOnly state', async () => {
+  skipTestIf(!options?.testCheckbox)(
+    '<BoolField> - renders a checkbox with correct readOnly state',
+    async () => {
       const onChange = jest.fn();
 
       renderWithZod({
@@ -342,11 +350,12 @@ export function testBoolField(
       await userEvent.click(element);
 
       expect(onChange).not.toHaveBeenCalled();
-    });
-  }
+    },
+  );
 
-  if (options?.testSwitch) {
-    test('<BoolField> - renders a switch with correct readOnly state', async () => {
+  skipTestIf(!options?.testSwitch)(
+    '<BoolField> - renders a switch with correct readOnly state',
+    async () => {
       const onChange = jest.fn();
 
       renderWithZod({
@@ -370,8 +379,8 @@ export function testBoolField(
       await userEvent.click(element);
 
       expect(onChange).not.toHaveBeenCalled();
-    });
-  }
+    },
+  );
 
   test('<BoolField> - renders an input with correct label (specified)', () => {
     renderWithZod({
@@ -384,8 +393,9 @@ export function testBoolField(
     expect(screen.getByText(/BoolFieldLabel.*/)).toBeInTheDocument();
   });
 
-  if (options?.testCheckbox) {
-    test('<BoolField> - renders a checkbox with correct label (specified)', () => {
+  skipTestIf(!options?.testCheckbox)(
+    '<BoolField> - renders a checkbox with correct label (specified)',
+    () => {
       renderWithZod({
         element: (
           <BoolField
@@ -399,11 +409,12 @@ export function testBoolField(
       });
 
       expect(screen.getByText(/BoolFieldLabel.*/)).toBeInTheDocument();
-    });
-  }
+    },
+  );
 
-  if (options?.testSwitch) {
-    test('<BoolField> - renders a switch with correct label (specified)', () => {
+  skipTestIf(!options?.testSwitch)(
+    '<BoolField> - renders a switch with correct label (specified)',
+    () => {
       renderWithZod({
         element: (
           <BoolField
@@ -417,9 +428,12 @@ export function testBoolField(
       });
 
       expect(screen.getByText(/BoolFieldLabel.*/)).toBeInTheDocument();
-    });
+    },
+  );
 
-    test('<BoolField> - renders a switch with correct legend (specified)', () => {
+  skipTestIf(!options?.testSwitch)(
+    '<BoolField> - renders a switch with correct legend (specified)',
+    () => {
       renderWithZod({
         element: (
           <BoolField
@@ -433,8 +447,8 @@ export function testBoolField(
       });
 
       expect(screen.getByText(/BoolFieldLegend.*/)).toBeInTheDocument();
-    });
-  }
+    },
+  );
 
   test('<BoolField> - renders an input with correct value (default)', () => {
     renderWithZod({
@@ -449,8 +463,9 @@ export function testBoolField(
     expect(element).not.toBeChecked();
   });
 
-  if (options?.testCheckbox) {
-    test('<BoolField> - renders a checkbox with correct value (default)', () => {
+  skipTestIf(!options?.testCheckbox)(
+    '<BoolField> - renders a checkbox with correct value (default)',
+    () => {
       renderWithZod({
         element: <BoolField name="x" data-testid="boolfield" checkbox />,
         schema: z.object({ x: z.boolean() }),
@@ -461,11 +476,12 @@ export function testBoolField(
         : screen.getByTestId('boolfield').querySelector('input');
 
       expect(element).not.toBeChecked();
-    });
-  }
+    },
+  );
 
-  if (options?.testSwitch) {
-    test('<BoolField> - renders a switch with correct value (default)', () => {
+  skipTestIf(!options?.testSwitch)(
+    '<BoolField> - renders a switch with correct value (default)',
+    () => {
       renderWithZod({
         element: (
           <BoolField name="x" data-testid="boolfield" appearance="switch" />
@@ -478,8 +494,8 @@ export function testBoolField(
         : screen.getByTestId('boolfield').querySelector('input');
 
       expect(element).not.toBeChecked();
-    });
-  }
+    },
+  );
 
   test('<BoolField> - renders an input with correct value (model)', () => {
     renderWithZod({
@@ -495,8 +511,9 @@ export function testBoolField(
     expect(element).toBeChecked();
   });
 
-  if (options?.testCheckbox) {
-    test('<BoolField> - renders a checkbox with correct value (model)', () => {
+  skipTestIf(!options?.testCheckbox)(
+    '<BoolField> - renders a checkbox with correct value (model)',
+    () => {
       renderWithZod({
         element: <BoolField name="x" data-testid="boolfield" checkbox />,
         model: { x: true },
@@ -508,11 +525,12 @@ export function testBoolField(
         : screen.getByTestId('boolfield').querySelector('input');
 
       expect(element).toBeChecked();
-    });
-  }
+    },
+  );
 
-  if (options?.testSwitch) {
-    test('<BoolField> - renders a switch with correct value (model)', () => {
+  skipTestIf(!options?.testSwitch)(
+    '<BoolField> - renders a switch with correct value (model)',
+    () => {
       renderWithZod({
         element: (
           <BoolField name="x" data-testid="boolfield" appearance="switch" />
@@ -526,8 +544,8 @@ export function testBoolField(
         : screen.getByTestId('boolfield').querySelector('input');
 
       expect(element).toBeChecked();
-    });
-  }
+    },
+  );
 
   test('<BoolField> - renders an input with correct value (specified)', () => {
     renderWithZod({
@@ -542,8 +560,9 @@ export function testBoolField(
     expect(element).toBeChecked();
   });
 
-  if (options?.testCheckbox) {
-    test('<BoolField> - renders a checkbox with correct value (specified)', () => {
+  skipTestIf(!options?.testCheckbox)(
+    '<BoolField> - renders a checkbox with correct value (specified)',
+    () => {
       renderWithZod({
         element: <BoolField name="x" data-testid="boolfield" value checkbox />,
         schema: z.object({ x: z.boolean() }),
@@ -554,11 +573,12 @@ export function testBoolField(
         : screen.getByTestId('boolfield').querySelector('input');
 
       expect(element).toBeChecked();
-    });
-  }
+    },
+  );
 
-  if (options?.testSwitch) {
-    test('<BoolField> - renders a switch with correct value (specified)', () => {
+  skipTestIf(!options?.testSwitch)(
+    '<BoolField> - renders a switch with correct value (specified)',
+    () => {
       renderWithZod({
         element: (
           <BoolField
@@ -576,8 +596,8 @@ export function testBoolField(
         : screen.getByTestId('boolfield').querySelector('input');
 
       expect(element).toBeChecked();
-    });
-  }
+    },
+  );
 
   test('<BoolField> - renders an input which correctly reacts on change', async () => {
     const onChange = jest.fn();
@@ -598,8 +618,9 @@ export function testBoolField(
     expect(onChange).toHaveBeenLastCalledWith('x', true);
   });
 
-  if (options?.testCheckbox) {
-    test('<BoolField> - renders a checkbox which correctly reacts on change', async () => {
+  skipTestIf(!options?.testCheckbox)(
+    '<BoolField> - renders a checkbox which correctly reacts on change',
+    async () => {
       const onChange = jest.fn();
       renderWithZod({
         element: <BoolField name="x" data-testid="boolfield" checkbox />,
@@ -616,11 +637,12 @@ export function testBoolField(
       await userEvent.click(element);
 
       expect(onChange).toHaveBeenLastCalledWith('x', true);
-    });
-  }
+    },
+  );
 
-  if (options?.testSwitch) {
-    test('<BoolField> - renders a switch which correctly reacts on change', async () => {
+  skipTestIf(!options?.testSwitch)(
+    '<BoolField> - renders a switch which correctly reacts on change',
+    async () => {
       const onChange = jest.fn();
       renderWithZod({
         element: (
@@ -639,8 +661,8 @@ export function testBoolField(
       await userEvent.click(element);
 
       expect(onChange).toHaveBeenLastCalledWith('x', true);
-    });
-  }
+    },
+  );
 
   test('<BoolField> - renders a wrapper with unknown props', () => {
     renderWithZod({
@@ -662,8 +684,9 @@ export function testBoolField(
     expect(field).toHaveAttribute('data-z', 'z');
   });
 
-  if (options?.testError) {
-    test('<BoolField> - renders correct error text (specified)', () => {
+  skipTestIf(!options?.testError)(
+    '<BoolField> - renders correct error text (specified)',
+    () => {
       const screen = renderWithZod({
         element: (
           <BoolField
@@ -679,9 +702,12 @@ export function testBoolField(
       expect(screen.container.querySelector('.red.label')).toHaveTextContent(
         'Error',
       );
-    });
+    },
+  );
 
-    test('<BoolField> - renders correct error text (showInlineError=false)', () => {
+  skipTestIf(!options?.testError)(
+    '<BoolField> - renders correct error text (showInlineError=false)',
+    () => {
       const screen = renderWithZod({
         element: (
           <BoolField
@@ -695,11 +721,12 @@ export function testBoolField(
       });
 
       expect(screen.container.querySelector('.red.label')).toBeNull();
-    });
-  }
+    },
+  );
 
-  if (options?.testInline) {
-    test('<BoolField> - renders an inline input', () => {
+  skipTestIf(!options?.testInline)(
+    '<BoolField> - renders an inline input',
+    () => {
       const screen = renderWithZod({
         element: <BoolField name="x" inline />,
         schema: z.object({ x: z.boolean() }),
@@ -709,11 +736,12 @@ export function testBoolField(
       expect(
         screen.container.querySelectorAll('.checkbox-inline'),
       ).toHaveLength(1);
-    });
-  }
+    },
+  );
 
-  if (options?.testMUIThemeProps) {
-    test('<BoolField> - default props are not passed when MUI theme props are specified', () => {
+  skipTestIf(!options?.testMUIThemeProps)(
+    '<BoolField> - default props are not passed when MUI theme props are specified',
+    () => {
       const theme = createMuiTheme({
         props: { MuiFormControl: { fullWidth: false, margin: 'normal' } },
       });
@@ -733,9 +761,12 @@ export function testBoolField(
 
       expect(elements).toHaveLength(1);
       expect(elements[0]).not.toHaveClass('MuiFormControl-fullWidth');
-    });
+    },
+  );
 
-    test('<BoolField> - default props are passed when MUI theme props are absent', () => {
+  skipTestIf(!options?.testMUIThemeProps)(
+    '<BoolField> - default props are passed when MUI theme props are absent',
+    () => {
       const theme = createMuiTheme({});
 
       const screen = renderWithZod({
@@ -753,9 +784,12 @@ export function testBoolField(
 
       expect(elements).toHaveLength(1);
       expect(elements[0]).toHaveClass('MuiFormControl-fullWidth');
-    });
+    },
+  );
 
-    test('<BoolField> - explicit props are passed when MUI theme props are specified', () => {
+  skipTestIf(!options?.testMUIThemeProps)(
+    '<BoolField> - explicit props are passed when MUI theme props are specified',
+    () => {
       const theme = createMuiTheme({
         props: { MuiFormControl: { fullWidth: true, margin: 'dense' } },
       });
@@ -780,6 +814,6 @@ export function testBoolField(
 
       expect(elements).toHaveLength(1);
       expect(elements[0]).not.toHaveClass('MuiFormControl-fullWidth');
-    });
-  }
+    },
+  );
 }
