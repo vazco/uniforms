44833e78990057fdae86f13f01d104b883f3560f
Piotr Pośpiech <ptkpospiech@gmail.com>
2024-02-16 13:51:41 +0100

    refactor

--- packages/uniforms/__suites__/QuickForm.tsx
+++ packages/uniforms/__suites__/QuickForm.tsx
@@ -1,10 +1,9 @@
 import { render } from '@testing-library/react';
-import React from 'react';
-import type { QuickForm as QuickFormType } from 'uniforms';
+import React, { ComponentType } from 'react';
 import { ZodBridge } from 'uniforms-bridge-zod';
 import z from 'zod';
 
-export function testQuickForm(QuickForm: QuickFormType) {
+export function testQuickForm(QuickForm: ComponentType<any>) {
   const bridge = new ZodBridge({ schema: z.object({}) });
 
   test('<QuickForm> - renders', () => {
