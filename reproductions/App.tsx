7440530af2d6c189f00ccdfd0014772148cf0ad6
Piotr Pośpiech <ptkpospiech@gmail.com>
2024-04-20 17:22:45 +0200

    removed uniforms-bridge-graphql

--- reproductions/App.tsx
+++ reproductions/App.tsx
@@ -9,7 +9,6 @@ import { AutoForm } from 'uniforms-mui';
 // import { AutoForm } from 'uniforms-semantic';
 
 // import { bridge as schema } from './schema/json-schema';
-// import { bridge as schema } from './schema/graphql-schema';
 // import { bridge as schema } from './schema/simple-schema-2';
 import { bridge as schema } from './schema/all-fields-schema';
 
