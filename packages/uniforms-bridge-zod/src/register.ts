028672a9b60cc53fe4b532bb0483b5e5aeb97bbf
Piotr Pośpiech <ptkpospiech@gmail.com>
2024-04-20 16:35:16 +0200

    added zod type - uniforms for props

--- packages/uniforms-bridge-zod/src/register.ts
+++ packages/uniforms-bridge-zod/src/register.ts
@@ -1,4 +1,5 @@
-import { filterDOMProps } from 'uniforms';
+import { ConnectedField, filterDOMProps, UnknownObject } from 'uniforms';
+import { z } from 'zod';
 
 // There's no possibility to retrieve them at runtime.
 declare module 'uniforms' {
@@ -9,3 +10,22 @@ declare module 'uniforms' {
 }
 
 filterDOMProps.register('minCount', 'maxCount');
+
+declare module 'zod' {
+  interface ZodType {
+    uniforms(uniforms: UnknownObject | ConnectedField<any>): this;
+  }
+
+  interface ZodTypeDef {
+    uniforms: UnknownObject | ConnectedField<any>;
+  }
+}
+
+z.ZodType.prototype.uniforms = function extend(uniforms) {
+  const This = (this as any).constructor;
+
+  return new This({
+    ...this._def,
+    uniforms,
+  });
+};
