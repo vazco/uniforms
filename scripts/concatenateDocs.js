8d71d98013cc60919bca90b9df63786709d6c83d
Konrad Bosak <konrad.bosak@proton.me>
2024-05-24 13:02:03 +0200

    Fix lint issues

--- scripts/concatenateDocs.js
+++ scripts/concatenateDocs.js
@@ -1,9 +1,13 @@
+/* eslint no-console: "off" */
+
 const fs = require('fs');
 const path = require('path');
 
 // Check if the correct number of arguments are passed
 if (process.argv.length !== 4) {
-  console.log(`Usage: node ${path.basename(__filename)} <root_directory> <output_file>`);
+  console.log(
+    `Usage: node ${path.basename(__filename)} <root_directory> <output_file>`,
+  );
   process.exit(1);
 }
 
@@ -13,7 +17,9 @@ const outputFile = process.argv[3];
 // Create or clear the output file
 fs.writeFileSync(outputFile, '');
 
-console.log(`Starting concatenation of md files from '${rootDirectory}' directory.`);
+console.log(
+  `Starting concatenation of md files from '${rootDirectory}' directory.`,
+);
 
 // Find all .md files and concatenate their contents
 function processDirectory(directory) {
@@ -21,7 +27,10 @@ function processDirectory(directory) {
     const fullPath = path.join(directory, dirent.name);
     if (dirent.isDirectory() && !['node_modules'].includes(dirent.name)) {
       processDirectory(fullPath);
-    } else if (dirent.isFile() && ['.md', '.mdx'].includes(path.extname(dirent.name))) {
+    } else if (
+      dirent.isFile() &&
+      ['.md', '.mdx'].includes(path.extname(dirent.name))
+    ) {
       console.log(`Adding file: ${fullPath}`);
       const data = fs.readFileSync(fullPath, 'utf8');
       fs.appendFileSync(outputFile, `File: ${fullPath}\n\n${data}\n`);
