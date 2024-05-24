1436d4c645fc77bf4120163bf8a7b4cce5a2f302
Piotr Pośpiech <37746259+piotrpospiech@users.noreply.github.com>
2024-05-24 11:32:33 +0200

    Deprecate `uniforms-bridge-graphql` package (#1322)

--- packages/uniforms-bridge-graphql/__tests__/index.ts
+++
@@ -1,8 +0,0 @@
-import * as uniformsGraphQL from 'uniforms-bridge-graphql';
-
-it('exports everything', () => {
-  expect(uniformsGraphQL).toEqual({
-    default: expect.any(Function),
-    GraphQLBridge: expect.any(Function),
-  });
-});
