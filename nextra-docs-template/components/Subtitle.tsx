3d42be6c6f4c710f3fd066932dc59245327d94db
Volodymyr Zakhovaiko <zaxovaiko@gmail.com>
2024-09-06 12:04:26 +0200

    Copied all pages into docs folder

---
+++ nextra-docs-template/components/Subtitle.tsx
@@ -0,0 +1,17 @@
+import classNames from 'classnames';
+import React from 'react';
+
+import styles from '../index.module.css';
+
+export type SubtitleProps = JSX.IntrinsicElements['p'];
+
+export function Subtitle({ children, className, ...props }: SubtitleProps) {
+  return (
+    <h3
+      {...props}
+      className={classNames(styles.text, styles['section-subtitle'], className)}
+    >
+      {children}
+    </h3>
+  );
+}
--- nextra-docs-template/theme.config.tsx
+++ nextra-docs-template/theme.config.tsx
@@ -1,18 +1,24 @@
-import React from 'react'
-import { DocsThemeConfig } from 'nextra-theme-docs'
+import { DocsThemeConfig } from 'nextra-theme-docs';
+import React from 'react';
 
 const config: DocsThemeConfig = {
-  logo: <span>My Project</span>,
+  logo: <b>uniforms</b>,
   project: {
-    link: 'https://github.com/shuding/nextra-docs-template',
+    link: 'https://github.com/vazco/uniforms',
   },
   chat: {
-    link: 'https://discord.com',
+    link: 'https://forminer.com/?utm_source=uniforms&utm_medium=Menu_CTA&utm_campaign=Forminer_uniforms_menu_CTA&utm_id=Forminer_uniforms_menu',
   },
-  docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
+  docsRepositoryBase: 'https://github.com/vazco/uniforms',
   footer: {
-    text: 'Nextra Docs Template',
+    text: (
+      <p>
+        Copyright © 2016 - 2024 Vazco.
+        <br />
+        All Rights Reserved.
+      </p>
+    ),
   },
-}
+};
 
-export default config
+export default config;
