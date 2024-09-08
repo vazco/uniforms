808d59224d0096d7a01201a4967b36f95e980fc8
Piotr Pośpiech <37746259+piotrpospiech@users.noreply.github.com>
2024-09-08 15:49:42 +0200

    updated typescript - uniforms-antd package

--- packages/uniforms-antd/src/ListField.tsx
+++ packages/uniforms-antd/src/ListField.tsx
@@ -81,6 +81,7 @@ function List({
           isValidElement(child)
             ? cloneElement(child, {
                 key: `${itemIndex}-${childIndex}`,
+                // @ts-expect-error FIXME: Object literal may only specify known properties, and 'name' does not exist in type 'Partial<unknown> & Attributes'.
                 name: child.props.name?.replace('$', '' + itemIndex),
                 labelCol,
                 wrapperCol,
