132da51634ad00b5f691e87ca241bc7f2b4dc4e6
Volodymyr Zakhovaiko <zaxovaiko@gmail.com>
2024-09-13 17:03:34 +0200

    Removed docosaurs

--- website/pages-parts/Tutorial/GuestFormBasic.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { AutoForm } from '../../lib/universal';
-
-import { bridge as schema } from './GuestSchema';
-
-export function GuestFormBasic() {
-  return <AutoForm schema={schema} onSubmit={console.log} />;
-}
