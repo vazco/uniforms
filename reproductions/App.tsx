9e692dc411284aaf8c2f2619148fe3612363d2d9
Piotr Pośpiech <37746259+piotrpospiech@users.noreply.github.com>
2024-09-13 14:04:35 +0200

    added reproductions

---
+++ reproductions/App.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+// import { AutoForm } from 'uniforms-unstyled';
+// import { AutoForm } from 'uniforms-antd';
+// import { AutoForm } from 'uniforms-bootstrap4';
+// import { AutoForm } from 'uniforms-bootstrap5';
+import { AutoForm } from 'uniforms-mui';
+// import { AutoForm } from 'uniforms-semantic';
+
+// import { bridge as schema } from './schema/json-schema';
+// import { bridge as schema } from './schema/simple-schema-2';
+import { bridge as schema } from './schema/all-fields-schema';
+
+export function App() {
+  return (
+    <AutoForm
+      schema={schema}
+      onSubmit={(model: any) => alert(JSON.stringify(model, null, 2))}
+    />
+  );
+}
---
+++ reproductions/schema/simple-schema-2.tsx
@@ -0,0 +1,12 @@
+import SimpleSchema from 'simpl-schema';
+import { SimpleSchema2Bridge } from 'uniforms-bridge-simple-schema-2';
+
+const schema = new SimpleSchema({
+  betterNumber: {
+    type: Number,
+    min: 5,
+    max: 50,
+  },
+});
+
+export const bridge = new SimpleSchema2Bridge({ schema });
