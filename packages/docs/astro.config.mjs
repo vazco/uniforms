a1370d98d58270da941b23fafb761f364beab450
Volodymyr Zakhovaiko <zaxovaiko@gmail.com>
2024-10-25 15:36:31 +0200

    Set static render

--- packages/docs/astro.config.mjs
+++ packages/docs/astro.config.mjs
@@ -4,6 +4,7 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
+  output: 'static',
   integrations: [
     starlight({
       title: 'uniforms',
