750f7cf76afb2456637c0a4a7d3a40c305ee2eda
Michał Zendran <zendran.m@gmail.com>
2024-11-06 13:54:35 +0100

    Migrating Discussions section

--- website/src/pages-parts/LandingPage/LandingPage.tsx
+++ website/src/pages-parts/LandingPage/LandingPage.tsx
@@ -4,7 +4,7 @@ import React from 'react';
 import styles from '../../css/index.module.css';
 // import { CommercialServices } from './CommercialServices';
 // import { Comparison } from './Comparison';
-// import { Discussions } from './Discussions';
+import { Discussions } from './Discussions';
 import { ExampleFullyCustomizable } from './ExampleFullyCustomizable';
 import { ExampleSimpleYetPowerful } from './ExampleSimpleYetPowerful';
 import { Footer } from './Footer';
@@ -27,7 +27,7 @@ export function LandingPage() {
         <div className="container">
           <ExampleSimpleYetPowerful />
           <ExampleFullyCustomizable />
-          {/* <Discussions /> */}
+          <Discussions />
         </div>
         {/* <WhoUses />
         <div className="container">
