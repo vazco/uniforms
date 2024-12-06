ef088ef94a06f0746c2157b543151e9cfebf86f0
Piotr Pośpiech <37746259+piotrpospiech@users.noreply.github.com>
2024-12-06 12:07:49 +0100

    Docs - Sandpack configuration (#1386)

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -2,6 +2,6 @@ module.exports = {
   '**/*.{js,ts,tsx}': [
     'eslint --cache --cache-location node_modules/.cache/eslint --cache-strategy content',
   ],
-  '**/*.{json,md,mdx,yml}': ['prettier --check'],
+  '**/*.{json,md,yml}': ['prettier --check'],
   '**/*.{ts,tsx}': () => 'pnpm lint:types',
 };
