6768543de37be50cfc9ec01d9ebcdb25017ea949
Piotr Pośpiech <37746259+piotrpospiech@users.noreply.github.com>
2025-01-17 15:08:38 +0100

    Added custom field example to docs (#1392)

--- website/components/CodeEditorTabs.tsx
+++ website/components/CodeEditorTabs.tsx
@@ -3,26 +3,24 @@ import { Bridges } from '@site/components/CodeEditor/dependencies';
 import TabItem from '@theme/TabItem';
 import Tabs from '@theme/Tabs';
 
-type CodeEditorTabsProps = {
-  zod: Record<string, string>;
-  json: Record<string, string>;
-  simple: Record<string, string>;
+type CodeEditorTabs = {
+  tabs: {
+    label: string;
+    value: string;
+    files: Record<string, string>;
+  }[];
 };
 
-export function CodeEditorTabs(props: CodeEditorTabsProps) {
-  const { zod, json, simple } = props;
+export function CodeEditorTabs(props: CodeEditorTabs) {
+  const { tabs } = props;
 
   return (
     <Tabs>
-      <TabItem value="zod" label="Zod" default>
-        <CodeEditor bridge={Bridges.Zod} files={zod} />
-      </TabItem>
-      <TabItem value="json-schema" label="JSON Schema">
-        <CodeEditor bridge={Bridges.JSONSchema} files={json} />
-      </TabItem>
-      <TabItem value="simpl-schema" label="Simple Schema">
-        <CodeEditor bridge={Bridges.SimpleSchema} files={simple} />
-      </TabItem>
+      {tabs.map(({ label, value, files }, index) => (
+        <TabItem key={value} value={value} label={label} default={index === 0}>
+          <CodeEditor bridge={Bridges.Zod} files={files} />
+        </TabItem>
+      ))}
     </Tabs>
   );
 }
