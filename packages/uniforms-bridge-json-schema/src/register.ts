0ee8696e28c9e08712871dcc57bf6de16b1f6bef
Michał Zendran <zendran.m@gmail.com>
2025-11-19 14:04:18 +0100

    Registering decimal prop in json schema bridge

--- packages/uniforms-bridge-json-schema/src/register.ts
+++ packages/uniforms-bridge-json-schema/src/register.ts
@@ -3,9 +3,10 @@ import { filterDOMProps } from 'uniforms';
 // There's no possibility to retrieve them at runtime.
 declare module 'uniforms' {
   interface FilterDOMProps {
+    decimal: never;
     minCount: never;
     maxCount: never;
   }
 }
 
-filterDOMProps.register('minCount', 'maxCount');
+filterDOMProps.register('decimal', 'minCount', 'maxCount');
